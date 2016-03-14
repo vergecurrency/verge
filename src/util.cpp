05084e44415bc21b98b22dc3068afa88049a5d20
vergecurrency <vergecurrency@hushmail.com>
2016-03-14 07:13:11 -0400

    Revert "clear path cache after getting a proper config file"
    This reverts commit 5c1ffe018a41180ffc66a4e62a57c55da0e8d3df.

--- src/util.cpp
+++ src/util.cpp
@@ -79,7 +79,6 @@ bool fNoListen = false;
 bool fLogTimestamps = false;
 CMedianFilter<int64> vTimeOffsets(200,0);
 bool fReopenDebugLog = false;
-bool fCachedPath[2] = {false, false};
 
 // Init OpenSSL library multithreading support
 static CCriticalSection** ppmutexOpenSSL;
@@ -1090,12 +1089,13 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific)
 
     static fs::path pathCached[2];
     static CCriticalSection csPathCached;
+    static bool cachedPath[2] = {false, false};
 
     fs::path &path = pathCached[fNetSpecific];
 
     // This can be called during exceptions by printf, so we cache the
     // value so we don't have to do memory allocations after that.
-    if (fcachedPath[fNetSpecific])
+    if (cachedPath[fNetSpecific])
         return path;
 
     LOCK(csPathCached);
@@ -1114,7 +1114,7 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific)
 
     fs::create_directory(path);
 
-    fcachedPath[fNetSpecific]=true;
+    cachedPath[fNetSpecific]=true;
     return path;
 }
 
@@ -1132,9 +1132,6 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
     if (!streamConfig.good())
         return; // No bitcoin.conf file is OK
 
-	// clear path cache after loading config file
-    fCachedPath[0] = fCachedPath[1] = false;	
-		
     set<string> setOptions;
     setOptions.insert("*");
 
