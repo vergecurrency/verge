905c56b527c4c4c9c0cc3792e4bde64c3f5b6889
vergecurrency <vergecurrency@hushmail.com>
2016-03-17 07:29:01 -0400

    New createmultisig rpc command
    This is to support the signrawtransaction API call; given the public
    keys involved in a multisig transaction, this gives back the
    redeemScript
    needed to sign it.

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -236,6 +236,7 @@ static const CRPCCommand vRPCCommands[] =
     { "sendfrom",               &sendfrom,               false,  false },
     { "sendmany",               &sendmany,               false,  false },
     { "addmultisigaddress",     &addmultisigaddress,     false,  false },
+	{ "createmultisig",         &createmultisig,         true,   true  },
     { "getrawmempool",          &getrawmempool,          true,   false },
     { "getblock",               &getblock,               false,  false },
     { "getblockbynumber",       &getblockbynumber,       false,  false },
@@ -1179,6 +1180,8 @@ Array RPCConvertValues(const std::string &strMethod, const std::vector<std::stri
     if (strMethod == "reservebalance"          && n > 1) ConvertTo<double>(params[1]);
     if (strMethod == "addmultisigaddress"     && n > 0) ConvertTo<boost::int64_t>(params[0]);
     if (strMethod == "addmultisigaddress"     && n > 1) ConvertTo<Array>(params[1]);
+	if (strMethod == "createmultisig"         && n > 0) ConvertTo<boost::int64_t>(params[0]);
+	if (strMethod == "createmultisig"         && n > 1) ConvertTo<Array>(params[1]);
     if (strMethod == "listunspent"            && n > 0) ConvertTo<boost::int64_t>(params[0]);
     if (strMethod == "listunspent"            && n > 1) ConvertTo<boost::int64_t>(params[1]);
     if (strMethod == "listunspent"            && n > 2) ConvertTo<Array>(params[2]);
