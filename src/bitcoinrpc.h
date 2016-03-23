c7e151cb59e06df2e64b76ffb638f706f8ae96fc
vergecurrency <vergecurrency@hushmail.com>
2016-03-23 06:08:08 -0400

    .

--- src/bitcoinrpc.h
+++ src/bitcoinrpc.h
@@ -198,7 +198,6 @@ extern json_spirit::Value signrawtransaction(const json_spirit::Array& params, b
 extern json_spirit::Value sendrawtransaction(const json_spirit::Array& params, bool fHelp);
 
 extern json_spirit::Value getblockcount(const json_spirit::Array& params, bool fHelp); // in rpcblockchain.cpp
-extern json_spirit::Value getblockvalue(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getdifficulty(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value settxfee(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getrawmempool(const json_spirit::Array& params, bool fHelp);
