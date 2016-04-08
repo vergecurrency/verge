4a7d338f397668e0e9df5be84827653b51c2a350
VERGE/XVG <vergecurrency@hushmail.com>
2016-04-08 05:02:40 -0400

    upnp update

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1239,7 +1239,7 @@ bool CWallet::CreateTransaction(const vector<pair<CScript, int64> >& vecSend, CW
         CTxDB txdb("r");
         {
             nFeeRet = nTransactionFee;
-            loop
+            while (true)
             {
                 wtxNew.vin.clear();
                 wtxNew.vout.clear();
@@ -1499,7 +1499,7 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
     }
 
     int64 nMinFee = 0;
-    loop
+    while (true)
     {
         // Set output amount
         if (txNew.vout.size() == 3)
