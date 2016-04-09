135318d982fde2d97ac44c518c08ca1591edb02d
VERGE/XVG <vergecurrency@hushmail.com>
2016-04-08 23:08:04 -0400

    mac build fix

--- src/serialize.h
+++ src/serialize.h
@@ -839,6 +839,7 @@ public:
     iterator insert(iterator it, const char& x=char()) { return vch.insert(it, x); }
     void insert(iterator it, size_type n, const char& x) { vch.insert(it, n, x); }
 
+#ifndef MAC_OSX
     void insert(iterator it, const_iterator first, const_iterator last)
     {
         assert(last - first >= 0);
@@ -851,7 +852,7 @@ public:
         else
             vch.insert(it, first, last);
     }
-
+#endif
     void insert(iterator it, std::vector<char>::const_iterator first, std::vector<char>::const_iterator last)
     {
         assert(last - first >= 0);
