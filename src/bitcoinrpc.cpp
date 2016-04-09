421c21d6d01acfdfb434d73dc3ae5cc357e25246
VERGE/XVG <vergecurrency@hushmail.com>
2016-04-08 20:14:52 -0400

    fixd rpcport

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -47,7 +47,7 @@ void ThreadRPCServer3(void* parg);
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 18393 : 46393;
+    return GetBoolArg("-testnet", false) ? 18393 : 20102;
 }
 
 Object JSONRPCError(int code, const string& message)
