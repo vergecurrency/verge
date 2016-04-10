1e69c29c36ab02318168a7320e8d8e443d4c781f
Mike Kinney <mike.kinney@gmail.com>
2016-04-10 01:02:19 -0700

    forgot to change BATTLESHIP

--- building/windows/build.sh
+++ building/windows/build.sh
@@ -5,11 +5,11 @@ set -xue
 
 MXE_DIR=/usr/lib/mxe
 
-if [ "$BATTLESHIP_PLATFORM" = "windows32" ]; then
+if [ "$VERGE_PLATFORM" = "windows32" ]; then
     MXE_TARGET=i686-w64-mingw32.static
 fi
 
-if [ "$BATTLESHIP_PLATFORM" = "windows64" ]; then
+if [ "$VERGE_PLATFORM" = "windows64" ]; then
     MXE_TARGET=x86_64-w64-mingw32.static
 fi
 
