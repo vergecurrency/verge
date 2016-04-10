4d8e2d1e7e49d8c4e32addf5313de3c45f2b04e1
Mike Kinney <mike.kinney@gmail.com>
2016-04-10 01:26:26 -0700

    see if we can build windows db and move linux requirements down to script

--- building/windows/requirements.sh
+++ building/windows/requirements.sh
@@ -11,6 +11,10 @@ sudo apt-get update
 
 sudo apt-get --yes install upx-ucl
 
+sudo apt-get --yes install p7zip-full autoconf automake autopoint bash bison bzip2 cmake flex gettext git g++ gperf intltool libffi-dev libtool libltdl-dev libssl-dev libxml-parser-perl make openssl patch perl pkg-config python ruby scons sed unzip wget xz-utils
+
+sudo apt-get --yes install g++-multilib libc6-dev-i386
+
 if [ "$VERGE_PLATFORM" = "windows32" ]; then
     MXE_TARGET=i686-w64-mingw32.static
 fi
@@ -23,8 +27,27 @@ MXE2_TARGET=$(echo "$MXE_TARGET" | sed 's/_/-/g')
 sudo apt-get --yes install \
     mxe-${MXE2_TARGET}-qt
 
-# MXE doesn't have 64bit NSIS
-#sudo apt-get --yes install \
-    #mxe-i686-w64-mingw32.static-nsis
+# build db
+cd /tmp
+wget http://download.oracle.com/otn/berkeley-db/db-5.3.28.tar.gz
+tar xzf db-5.3.28.tar.gz
+cd /tmp/db-5.3.28
+touch compile-db.sh
+chmod ugo+x compile-db.sh
+sed -i "s/WinIoCtl.h/winioctl.h/g" src/dbinc/win_db.h
+mkdir build_mxe
+cd build_mxe
+
+CC=${MXE_TARGET} \
+CXX=${MXE_TARGET} \
+../dist/configure \
+	--disable-replication \
+	--enable-mingw \
+	--enable-cxx \
+	--host x86 \
+	--prefix=${MXE_TARGET}
+
+make
+make install
 
 set +xue
