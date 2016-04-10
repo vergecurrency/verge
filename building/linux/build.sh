175c80f6408f8331b1467b946940f270a1d0b4fe
Mike Kinney <mike.kinney@gmail.com>
2016-04-10 00:57:22 -0700

    see if we can loop thru different env

---
+++ building/linux/build.sh
@@ -0,0 +1,3 @@
+./autogen.sh
+./configure
+make
---
+++ building/windows/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+set -xue
+
+MXE_DIR=/usr/lib/mxe
+
+if [ "$BATTLESHIP_PLATFORM" = "windows32" ]; then
+    MXE_TARGET=i686-w64-mingw32.static
+fi
+
+if [ "$BATTLESHIP_PLATFORM" = "windows64" ]; then
+    MXE_TARGET=x86_64-w64-mingw32.static
+fi
+
+echo "TODO: build for windows goes here..."
+#${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir
+#cmake --build build-dir --config Release
+
+set +xue
