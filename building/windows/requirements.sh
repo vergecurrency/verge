36fa2106cb8d2fabd6d552c59c497e12a3c44f73
Mike Kinney <mike.kinney@gmail.com>
2016-04-11 23:49:30 -0700

    try without update/upgrade

--- building/windows/requirements.sh
+++ building/windows/requirements.sh
@@ -4,14 +4,14 @@ pwd
 
 # using: https://github.com/coinzen/devcoin/blob/master/doc/build-mingw-under_linux.txt as a guide
 
-sudo apt-get --yes upgrade 
-sudo apt-get --yes update
+#sudo apt-get --yes upgrade 
+#sudo apt-get --yes update
 sudo apt-get --yes install git sudo make mingw-w64 mingw-w64-common wget g++-mingw-w64 binutils-mingw-w64-x86-64 g++-mingw-w64-x86-64 gcc-mingw-w64-x86-64 mingw-w64-tools mingw-w64-x86-64-dev
 
 sudo apt-get --yes install software-properties-common
 sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo add-apt-repository --yes ppa:bitcoin/bitcoin
-sudo apt-get --yes update -qq
+#sudo apt-get --yes update -qq
 
 # build openssl
 cd /tmp/
