8609c0c359a78fd99250244425a155ed1472f6b1
Mike Kinney <mike.kinney@gmail.com>
2016-04-13 21:30:23 -0700

    need to build boost system

--- building/windows/build.sh
+++ building/windows/build.sh
@@ -2,7 +2,8 @@
 
 ./autogen.sh --host=i686-w64-mingw32
 
-env BOOST_ROOT='/tmp/boost_1_60_0' CPPFLAGS='-I/tmp/openssl-1.0.1f/include -I/tmp/db-4.8.30/build_unix' LDFLAGS='-L/tmp/openssl-1.0.1f -L/tmp/db-4.8.30/build_unix/.libs/' ./configure --host=i686-w64-mingw32 
+#env BOOST_ROOT='/tmp/boost_1_60_0' 
+env CPPFLAGS='-I/tmp/openssl-1.0.1f/include -I/tmp/db-4.8.30/build_unix -I/tmp/boost_1_60_0' LDFLAGS='-L/tmp/openssl-1.0.1f -L/tmp/db-4.8.30/build_unix/.libs -L/tmp/boost_1_60_0/stage/lib' ./configure --host=i686-w64-mingw32 
 # TODO: need? --enable-cxx
 
 make
