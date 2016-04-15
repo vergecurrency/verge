833e265f3bfd7a3e6ce9679730b7ff7b8fde33b5
Mike Kinney <mike.kinney@gmail.com>
2016-04-14 21:04:49 -0700

    attempt to get windows going

--- building/windows/req_boost.sh
+++ building/windows/req_boost.sh
@@ -10,12 +10,7 @@ cd /tmp/boost_1_55_0
 # (if troubleshooting and want to 'make clean all')
 # bjam --clean 
 
-./bootstrap.sh --without-icu --with-libraries=thread --with-libraries=system --with-libraries=test
+./bootstrap.sh --without-icu  
 echo "using gcc : 4.9.2 : i686-w64-mingw32-g++ : <rc>i686-w64-mingw32-windres <archiver>i686-w64-mingw32-ar ;" > user-config.jam
-sed -i 's/ -Wunused-local-typedefs//' ./libs/core/test/Jamfile.v2
-sed -i 's/40700/10/' ./boost/tuple/detail/tuple_basic.hpp
-sed -i 's/#.*//' libs/context/src/unsupported.cpp
-./b2 -j10 --user-config=user-config.jam toolset=gcc-mingw address-model=32 binary-format=pe target-os=windows release --prefix=/usr/i686-w64-mingw32/local --without-python --without-wave --without-context --without-coroutine --without-mpi --without-test --without-graph --without-graph_parallel -sNO_BZIP2=1
-sed -i 's/ -Wunused-local-typedefs//' ./libs/core/test/Jamfile.v2
-./b2 -j10 --user-config=user-config.jam toolset=gcc-mingw address-model=32 binary-format=pe target-os=windows release --prefix=/usr/i686-w64-mingw32/local --without-python --without-wave --without-context --without-coroutine --without-mpi --without-test --without-graph --without-graph_parallel -sNO_BZIP2=1
+./b2 --user-config=user-config.jam toolset=gcc-mingw binary-format=pe target-os=windows release --prefix=/usr/i686-w64-mingw32/local --without-python --without-wave --without-context --without-coroutine --without-mpi --without-graph --without-graph_parallel -sNO_BZIP2=1 threadapi=win32
 
