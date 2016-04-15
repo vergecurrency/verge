c97739f2830ca0e1bec8ea7a475c9e24e0d70526
Mike Kinney <mike.kinney@gmail.com>
2016-04-15 00:02:26 -0700

    builds on my machine

--- building/windows/req_openssl.sh
+++ building/windows/req_openssl.sh
@@ -6,8 +6,9 @@ cd /tmp/
 sudo apt-get source openssl
 # TODO: need to determine directory/version automatically
 cd /tmp/openssl-1.0.1f
-CROSS_COMPILE="x86_64-w64-mingw32-" ./Configure mingw64 no-zlib no-asm no-shared no-dso no-asm --prefix=/usr/i686-w64-mingw32
-PATH=$PATH:/usr/i686-w64-mingw32/bin make depend
-PATH=$PATH:/usr/i686-w64-mingw32/bin make
-PATH=$PATH:/usr/i686-w64-mingw32/bin sudo make install
+CROSS_COMPILE="x86_64-w64-mingw32-" ./Configure mingw64 no-zlib no-asm no-shared no-dso no-asm --prefix=/usr/i686-w64-mingw32  
+make CC=i686-w64-mingw32-gcc RANLIB=i686-w64-mingw32-ranlib
+#PATH=$PATH:/usr/i686-w64-mingw32/bin make depend
+#PATH=$PATH:/usr/i686-w64-mingw32/bin make
+#PATH=$PATH:/usr/i686-w64-mingw32/bin sudo make install
 
