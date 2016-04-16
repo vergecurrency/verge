783b15759715465c2abab11587a9d0b36226c694
Mike Kinney <mike.kinney@gmail.com>
2016-04-16 01:11:24 -0700

    need autoconf

--- building/windows/build.sh
+++ building/windows/build.sh
@@ -6,3 +6,5 @@
 PATH=$PATH:/usr/i686-w64-mingw32/bin env SSL_CFLAGS='-L/tmp/openssl-1.0.1f/include' SSL_LIBS='-L/tmp/openssl-1.0.1f/lib' CFLAGS='-I/tmp/openssl-1.0.1f/include -I/tmp/db-4.8.30/build_unix -I/tmp/boost_1_55_0 -I/tmp/miniupnpc-1.6' LDFLAGS='-L/tmp/openssl-1.0.1f -L/tmp/db-4.8.30/build_unix/.libs -L/tmp/boost_1_55_0/stage/lib' CRYPTO_CFLAGS='-L/tmp/openssl-1.0.1f/include' CRYPTO_LIBS='-L/tmp/openssl-1.0.1f/lib' CPPFLAGS='-I/tmp/openssl-1.0.1f/include -I/tmp/db-4.8.30/build_unix -I/tmp/boost_1_55_0' LDFLAGS='-L/tmp/openssl-1.0.1f -L/tmp/db-4.8.30/build_unix/.libs -L/tmp/boost_1_55_0/stage/lib -L/tmp/miniupnpc-1.6 -lssl -lcrypto' ./configure --host=i686-w64-mingw32 --with-boost-libdir=/tmp/boost_1_55_0/stage/lib 
 
 PATH=$PATH:/usr/i686-w64-mingw32/bin AR=i686-w64-mingw32-ar make
+
+echo "done"
