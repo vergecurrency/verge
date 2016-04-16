351de17bea056f474959ea327965d463602ae474
Mike Kinney <mike.kinney@gmail.com>
2016-04-16 12:11:15 -0700

    see if we can figure out who I am

--- building/common.sh
+++ building/common.sh
@@ -1,10 +1,15 @@
 
 # print out enviroment variables
+echo "=== Here are all of the environment variables:"
 env
+echo "==="
 
 PWD=`pwd`
 echo "PWD:$PWD"
+# This should be populated... oh well
 echo "DEPLOY_REPO_SLUG:${DEPLOY_REPO_SLUG}"
-ME=` echo "${DEPLOY_REPO_SLUG}/b" | sed 's!\(.*\)\/.*!\1!'`
+#ME=` echo "${DEPLOY_REPO_SLUG}/b" | sed 's!\(.*\)\/.*!\1!'`
+ME=` pwd | sed 's!.*/\(.*\)/VERGE!\1!'
 echo "ME:$ME"
 
+echo "==="
