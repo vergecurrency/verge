2662c32664ca6d4f1c5a83183339eee2abafe35b
Mike Kinney <mike.kinney@gmail.com>
2016-04-16 12:16:27 -0700

    neeed another tick

--- building/common.sh
+++ building/common.sh
@@ -7,9 +7,8 @@ echo "==="
 PWD=`pwd`
 echo "PWD:$PWD"
 # This should be populated... oh well
-echo "DEPLOY_REPO_SLUG:${DEPLOY_REPO_SLUG}"
-#ME=` echo "${DEPLOY_REPO_SLUG}/b" | sed 's!\(.*\)\/.*!\1!'`
-ME=` pwd | sed 's!.*/\(.*\)/VERGE!\1!'
+#echo "DEPLOY_REPO_SLUG:${DEPLOY_REPO_SLUG}"
+ME=`pwd | sed 's!.*/\(.*\)/VERGE!\1!'`
 echo "ME:$ME"
 
 echo "==="
