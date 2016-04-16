0770a1007d4c864f5c0e977c7d6a4244ae7cab90
Mike Kinney <mike.kinney@gmail.com>
2016-04-16 00:01:14 -0700

    adding sudo to openssl build

--- building/windows/req_openssl.sh
+++ building/windows/req_openssl.sh
@@ -7,8 +7,8 @@ sudo apt-get source openssl
 # TODO: need to determine directory/version automatically
 cd /tmp/openssl-1.0.1f
 #CROSS_COMPILE="x86_64-w64-mingw32-" sudo ./Configure mingw64 no-asm no-shared --prefix=/usr/i686-w64-mingw32  
-CROSS_COMPILE="x86_64-w64-mingw32-" ./Configure mingw64 no-asm shared --prefix=/opt/mingw64
-PATH=$PATH:/usr/i686-w64-mingw32/bin make depend
-PATH=$PATH:/usr/i686-w64-mingw32/bin make 
+CROSS_COMPILE="x86_64-w64-mingw32-" sudo ./Configure mingw64 no-asm shared --prefix=/opt/mingw64
+#PATH=$PATH:/usr/i686-w64-mingw32/bin make depend
+PATH=$PATH:/usr/i686-w64-mingw32/bin sudo make 
 
 echo "=== done building OPENSSL =="
