ba5f009eeb970313b68fd5ae8c6bcf8839052052
Mike Kinney <mike.kinney@gmail.com>
2016-04-16 14:34:05 -0700

    adding rvm reload; add option to get qt5 to build on windows

--- building/windows/dist.sh
+++ building/windows/dist.sh
@@ -7,5 +7,6 @@ sudo apt-get --yes install ruby curl
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
+rvm reload
 rvm install 2.2.3
 rvm use 2.2.3
--- building/windows/req_qt.sh
+++ building/windows/req_qt.sh
@@ -10,6 +10,6 @@ wget http://download.qt.io/official_releases/qt/5.5/5.5.0/single/qt-everywhere-o
 tar xzf qt-everywhere-opensource-src-5.5.0.tar.gz
 cd /tmp/qt-everywhere-opensource-src-5.5.0
 # Don't think we want this: '-no-openssl'
-./configure -prefix $PWD/install-release-static -static -confirm-license -release -opensource -no-sql-sqlite -no-audio-backend -nomake examples -nomake tests -no-sse2 -xplatform win32-g++ -device-option CROSS_COMPILE=i686-w64-mingw32- -skip qtactiveqt -skip qtmultimedia -skip qtdoc -skip qtcanvas3d -skip qtactiveqt -skip qtenginio -skip qtlocation -skip qtmultimedia -skip qtserialport -skip qtquick1 -skip qtquickcontrols -skip qtscript -skip qtsensors -skip qtwebkit -skip qtwebsockets -skip qtxmlpatterns
+./configure -prefix $PWD/install-release-static -static -confirm-license -release -opensource -no-sql-sqlite -no-audio-backend -nomake examples -nomake tests -no-sse2 -xplatform win32-g++ -device-option CROSS_COMPILE=i686-w64-mingw32- -skip qtactiveqt -skip qtmultimedia -skip qtdoc -skip qtcanvas3d -skip qtactiveqt -skip qtenginio -skip qtlocation -skip qtmultimedia -skip qtserialport -skip qtquick1 -skip qtquickcontrols -skip qtscript -skip qtsensors -skip qtwebkit -skip qtwebsockets -skip qtxmlpatterns -system-freetype  -fontconfig
 PATH=$PATH:/usr/i686-w64-mingw32/bin make
 
