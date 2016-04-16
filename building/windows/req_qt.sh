27d546d769c63707a537b58a6e3a13a5fd81e0a1
Mike Kinney <mike.kinney@gmail.com>
2016-04-16 15:17:16 -0700

    trying out mxe for cross compiling qt/wallet on windows

--- building/windows/req_qt.sh
+++ building/windows/req_qt.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# qt4  (see https://wiki.qt.io/MinGW-64-bit )
-# considering... https://sourceforge.net/projects/mingwbuilds/files/mingw-sources/4.8.1/src-4.8.1-release-rev5.tar.7z/download
-# also considering using the apt-get source ...
-echo "=== Building QT now..."
-sudo apt-get --yes install unzip libqt5-dev
-cd /tmp
-wget http://download.qt.io/official_releases/qt/5.5/5.5.0/single/qt-everywhere-opensource-src-5.5.0.tar.gz
-tar xzf qt-everywhere-opensource-src-5.5.0.tar.gz
-cd /tmp/qt-everywhere-opensource-src-5.5.0
-# Don't think we want this: '-no-openssl'
-./configure -prefix $PWD/install-release-static -static -confirm-license -release -opensource -no-sql-sqlite -no-audio-backend -nomake examples -nomake tests -no-sse2 -xplatform win32-g++ -device-option CROSS_COMPILE=i686-w64-mingw32- -skip qtactiveqt -skip qtmultimedia -skip qtdoc -skip qtcanvas3d -skip qtactiveqt -skip qtenginio -skip qtlocation -skip qtmultimedia -skip qtserialport -skip qtquick1 -skip qtquickcontrols -skip qtscript -skip qtsensors -skip qtwebkit -skip qtwebsockets -skip qtxmlpatterns -system-freetype  -fontconfig
-PATH=$PATH:/usr/i686-w64-mingw32/bin make
+echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" > /etc/apt/sources.list.d/mxeapt.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
+apt-get update
+apt-get install mxe-i686-w64-mingw32.static-qt
 
