17eb307663b3b2e068839d78f630dd29c5098425
Mike Kinney <mike.kinney@gmail.com>
2016-04-16 11:46:50 -0700

    get an okay-enough version of ruby for release publishing

--- building/linux/dist.sh
+++ building/linux/dist.sh
@@ -3,8 +3,10 @@ cp src/VERGEd .
 zip release.zip VERGEd
 
 # for pushing releases
-sudo apt-add-repository --yes ppa:brightbox/ruby-ng
-sudo apt-get update -qq
-sudo apt-get --yes install ruby
-rvm use ruby-2.2.3
+sudo apt-get --yes install ruby curl
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
+source /etc/profile.d/rvm.sh
+rvm install 2.2.3
+rvm use 2.2.3
 
--- building/mac/dist.sh
+++ building/mac/dist.sh
@@ -4,4 +4,3 @@ zip release.zip VERGEd
 
 # for pushing releases
 brew install ruby
-rvm use ruby-2.0.0-p643
