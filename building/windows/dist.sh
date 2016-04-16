c7368902ff4b885b1e7a4c555cab608ef699a750
Mike Kinney <mike.kinney@gmail.com>
2016-04-16 15:38:13 -0700

    make apt-get not so verbose; change the names of the release files; fix chmod/sudo on two windows build steps

--- building/windows/dist.sh
+++ building/windows/dist.sh
@@ -3,7 +3,7 @@ cp src/VERGEd.exe .
 zip release_${VERGE_PLATFORM}.zip VERGEd.exe
 
 # for pushing releases
-sudo apt-get --yes install ruby curl
+sudo apt-get --yes -qq install ruby curl
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
--- building/windows/req_qt.sh
+++ building/windows/req_qt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" > /etc/apt/sources.list.d/mxeapt.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
-apt-get update
-apt-get install mxe-i686-w64-mingw32.static-qt
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
+sudo apt-get update -qq
+sudo apt-get install --yes -qq mxe-i686-w64-mingw32.static-qt
 
