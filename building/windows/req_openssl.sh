0502fc64124352a91051380ca46f734b62931139
Mike Kinney <mike.kinney@gmail.com>
2016-04-17 06:46:47 -0700

    req_openssl.sh

--- building/windows/req_openssl.sh
+++ building/windows/req_openssl.sh
@@ -7,6 +7,8 @@ sudo apt-get source openssl > /dev/null
 # TODO: need to determine directory/version automatically
 cd /tmp/openssl-1.0.1f
 CROSS_COMPILE="x86_64-w64-mingw32-" sudo ./Configure mingw64 no-zlib no-asm no-shared no-dso no-asm --prefix=/usr/i686-w64-mingw32  
+# skip the tests as they build exe files that will not run on linux
+sed -i 's/TESTS = alltests/TESTS = /' Makefile
 sudo make CC=i686-w64-mingw32-gcc 
 
 echo "=== done building OPENSSL =="
