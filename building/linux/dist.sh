e18230fee1bcf4fc88cb6cf4b880a156a9a7c92e
Mike Kinney <mike.kinney@gmail.com>
2016-04-17 10:26:28 -0700

    strip exe (if it ever gets built!); install another ruby ver to see if release pub will work

--- building/linux/dist.sh
+++ building/linux/dist.sh
@@ -12,5 +12,6 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 source /etc/profile.d/rvm.sh
 rvm reload
 rvm install 2.2.3
+rvm install 1.9.3
 rvm use 2.2.3
 export PATH=/usr/local/rvm/gems/ruby-2.2.3/bin:$PATH
--- building/windows/dist.sh
+++ building/windows/dist.sh
@@ -1,7 +1,8 @@
 
 cp src/VERGEd.exe .
 cp src/VERGEd-qt.exe .
-# TODO: strip .exe?
+strip VERGEd.exe
+strip VERGEd-qt.exe
 zip release_${VERGE_PLATFORM}.zip VERGEd.exe VERGEd-qt.exe
 
 # for pushing releases
@@ -11,5 +12,6 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 source /etc/profile.d/rvm.sh
 rvm reload
 rvm install 2.2.3
+rvm install 1.9.3
 rvm use 2.2.3
 export PATH=/usr/local/rvm/gems/ruby-2.2.3/bin:$PATH
