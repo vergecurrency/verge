aa5cecab100d6f4beed9afd8c86dcbfa37178edf
Mike Kinney <mike.kinney@gmail.com>
2016-04-17 14:34:20 -0700

    bump qt5 to 5.6.0 on windows

--- building/windows/req_openssl.sh
+++ building/windows/req_openssl.sh
@@ -4,12 +4,13 @@
 echo "=== Building OPENSSL now..."
 cd /tmp/
 sudo apt-get source openssl > /dev/null
-# TODO: need to determine directory/version automatically
-cd /tmp/openssl-1.0.1f
-PATH=/usr/lib/mxe/usr/bin/:/usr/bin:$PATH CROSS_COMPILE="i686-w64-mingw32-" sudo ./Configure mingw64 no-zlib no-asm no-shared no-dso no-asm --prefix=/usr/i686-w64-mingw32  
+openssl_version=`dpkg -s openssl | grep Version | sed 's/Version: \(.*\)-.*/\1/'`
+cd /tmp/openssl-${openssl_version}
+PATH=/usr/lib/mxe/usr/bin/:/usr/bin:$PATH CROSS_COMPILE="i686-w64-mingw32-" sudo ./Configure mingw64 no-zlib no-asm no-shared no-dso no-asm --prefix=/usr/i686-w64-mingw32
 # skip the tests as they build exe files that will not run on linux
 sudo sed -i 's/TESTS = alltests/TESTS = /' Makefile
 sudo rm -rf certs/demo
 PATH=/usr/lib/mxe/usr/bin/:/usr/bin:$PATH sudo make CC=i686-w64-mingw32-gcc
+PATH=/usr/lib/mxe/usr/bin/:/usr/bin:$PATH sudo make CC=i686-w64-mingw32-gcc install
 
 echo "=== done building OPENSSL =="
--- building/windows/req_qt.sh
+++ building/windows/req_qt.sh
@@ -2,12 +2,10 @@
 
 echo "=== Building QT now..."
 cd /tmp/
-
-#sudo apt-get --yes install unzip libqt5-dev > /dev/null
-wget http://download.qt.io/official_releases/qt/5.5/5.5.0/single/qt-everywhere-opensource-src-5.5.0.tar.gz
-tar xzf qt-everywhere-opensource-src-5.5.0.tar.gz
-cd /tmp/qt-everywhere-opensource-src-5.5.0
-PATH=/usr/lib/mxe/usr/bin/:/usr/bin:$PATH CPPFLAGS="/usr/lib/mxe/usr/i686-w64-mingw32.shared/include/freetype2" ./configure -prefix $PWD/install-release-static -static -confirm-license -release -opensource -no-sql-sqlite -no-audio-backend -nomake examples -nomake tests -no-sse2 -xplatform win32-g++ -device-option CROSS_COMPILE=i686-w64-mingw32.static- -skip qtactiveqt -skip qtmultimedia -skip qtdoc -skip qtcanvas3d -skip qtactiveqt -skip qtenginio -skip qtlocation -skip qtmultimedia -skip qtserialport -skip qtquick1 -skip qtquickcontrols -skip qtscript -skip qtsensors -skip qtwebkit -skip qtwebsockets -skip qtxmlpatterns -system-freetype  -fontconfig
+wget http://download.qt.io/official_releases/qt/5.6/5.6.0/single/qt-everywhere-opensource-src-5.6.0.tar.gz
+tar xzf qt-everywhere-opensource-src-5.6.0.tar.gz
+cd /tmp/qt-everywhere-opensource-src-5.6.0
+PATH=/usr/lib/mxe/usr/bin/:/usr/bin:$PATH CPPFLAGS="/usr/lib/mxe/usr/i686-w64-mingw32.shared/include/freetype2" ./configure -prefix $PWD/install-release-static -static -confirm-license -release -opensource -no-sql-sqlite -no-audio-backend -nomake examples -nomake tests -no-sse2 -xplatform win32-g++ -device-option CROSS_COMPILE=i686-w64-mingw32.static- -skip qtactiveqt -skip qtmultimedia -skip qtdoc -skip qtcanvas3d -skip qtactiveqt -skip qtenginio -skip qtlocation -skip qtmultimedia -skip qtserialport -skip qtquickcontrols -skip qtscript -skip qtsensors -skip qtwebsockets -skip qtxmlpatterns -system-freetype -fontconfig
 PATH=/usr/lib/mxe/usr/bin/:/usr/bin:$PATH make
 PATH=/usr/lib/mxe/usr/bin/:/usr/bin:$PATH make install
 
