3a3a41927daafe9c86001d9a4a9290f670f4f204
Mike Kinney <mike.kinney@gmail.com>
2016-04-17 08:47:21 -0700

    trying to get db built for windows

--- building/windows/req_dbd.sh
+++ building/windows/req_dbd.sh
@@ -7,7 +7,7 @@ cd /tmp
 wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
 tar xzf db-4.8.30.NC.tar.gz
 cd /tmp/db-4.8.30.NC/build_unix
-sed -i 's/#include <direct.h>//' ../dist/../dbinc/win_db.h
-PATH=$PATH:/usr/i686-w64-mingw32/bin sh ../dist/configure --enable-cxx --enable-mingw --disable-replication
-PATH=$PATH:/usr/i686-w64-mingw32/bin make
-echo "=== done bilding DB =="
+../dist/configure --enable-cxx --enable-mingw --disable-replication --disable-shared --host=x86_64-w64-mingw32
+make library_build
+make install_lib install_include
+echo "=== done building DB =="
