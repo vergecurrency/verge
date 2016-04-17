fbd918da3bce65c774dece560773a6389fc4278d
Mike Kinney <mike.kinney@gmail.com>
2016-04-16 19:26:13 -0700

    need to use sudo/tee trick to write to root owned file

--- building/windows/req_qt.sh
+++ building/windows/req_qt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" > /etc/apt/sources.list.d/mxeapt.list
+echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mxeapt.list > /dev/null
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
 sudo apt-get -qq update > /dev/null
 sudo apt-get -qq --yes install mxe-i686-w64-mingw32.static-qt > /dev/null
