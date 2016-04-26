0adfdfde3dcd437e727a4297ad7c515a14daca7e
Mike Kinney <mike.kinney@gmail.com>
2016-04-25 19:44:40 -0700

    add the VERGE-qt (mac) into the release zip

--- building/mac/dist.sh
+++ building/mac/dist.sh
@@ -1,7 +1,9 @@
 
 cp src/VERGEd .
+cp src/qt/VERGE-qt .
 strip VERGEd
-zip release_${VERGE_PLATFORM}.zip VERGEd 
+strip VERGE-qt
+zip release_${VERGE_PLATFORM}.zip VERGEd VERGE-qt
 # TODO: Not sure how to do the 'make deploy' from travis... may not be possible :-(
 
 # for pushing releases
