2cbf5b6759260c0146623eb7a4a1299f2a14716e
Mike Kinney <mike.kinney@gmail.com>
2016-04-30 14:23:20 -0700

    change fee confirm message

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -683,10 +683,7 @@ void BitcoinGUI::closeEvent(QCloseEvent *event)
 void BitcoinGUI::askFee(qint64 nFeeRequired, bool *payFee)
 {
     QString strMessage =
-        tr("This transaction is over the size limit.  You can still send it for a fee of %1. "
-           "The fee amount will be destroyed, thus taken out of circulation, which is meant to "
-           "increase incentive to both miners and stake holders as transaction volume increases "
-           "over time. Do you want to pay the fee?").arg(
+        tr("Do you want to pay the %1 fee?").arg(
                 BitcoinUnits::formatWithUnit(BitcoinUnits::BTC, nFeeRequired));
     QMessageBox::StandardButton retval = QMessageBox::question(
           this, tr("Confirm transaction fee"), strMessage,
