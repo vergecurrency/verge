c84a693944ce7ec36fdb2a9ded1c651eb3d2b735
VERGE/XVG <vergecurrency@hushmail.com>
2016-09-17 16:03:39 -0400

    Create chainparamsbase.cpp

---
+++ src/chainparamsbase.cpp
@@ -0,0 +1,94 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "chainparamsbase.h"
+
+#include "util.h"
+
+#include <assert.h>
+
+#include <boost/assign/list_of.hpp>
+
+using namespace boost::assign;
+
+/**
+ * Main network
+ */
+class CBaseMainParams : public CBaseChainParams
+{
+public:
+    CBaseMainParams()
+    {
+        networkID = CBaseChainParams::MAIN;
+        nRPCPort = 20102;
+    }
+};
+static CBaseMainParams mainParams;
+
+/**
+ * Testnet (v3)
+ */
+class CBaseTestNetParams : public CBaseMainParams
+{
+public:
+    CBaseTestNetParams()
+    {
+        networkID = CBaseChainParams::TESTNET;
+        nRPCPort = 21102;
+        strDataDir = "testnet";
+    }
+};
+static CBaseTestNetParams testNetParams;
+
+
+static CBaseChainParams* pCurrentBaseParams = 0;
+
+const CBaseChainParams& BaseParams()
+{
+    assert(pCurrentBaseParams);
+    return *pCurrentBaseParams;
+}
+
+void SelectBaseParams(CBaseChainParams::Network network)
+{
+    switch (network) {
+    case CBaseChainParams::MAIN:
+        pCurrentBaseParams = &mainParams;
+        break;
+    case CBaseChainParams::TESTNET:
+        pCurrentBaseParams = &testNetParams;
+        break;
+    default:
+        assert(false && "Unimplemented network");
+        return;
+    }
+}
+
+CBaseChainParams::Network NetworkIdFromCommandLine()
+{
+    bool fRegTest = GetBoolArg("-regtest", false);
+    bool fTestNet = GetBoolArg("-testnet", false);
+
+    if (fTestNet && fRegTest)
+        return CBaseChainParams::MAX_NETWORK_TYPES;
+    if (fTestNet)
+        return CBaseChainParams::TESTNET;
+    return CBaseChainParams::MAIN;
+}
+
+bool SelectBaseParamsFromCommandLine()
+{
+    CBaseChainParams::Network network = NetworkIdFromCommandLine();
+    if (network == CBaseChainParams::MAX_NETWORK_TYPES)
+        return false;
+
+    SelectBaseParams(network);
+    return true;
+}
+
+bool AreBaseParamsConfigured()
+{
+    return pCurrentBaseParams != NULL;
+}
