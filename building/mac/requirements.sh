b7e75f4e5703e0f43e89b56176eb282e5360aa41
badbrainIRC <egypte333@hotmail.com>
2017-07-25 05:33:21 -0400

    Update requirements.sh

--- building/mac/requirements.sh
+++ building/mac/requirements.sh
@@ -1,7 +1,5 @@
 #brew install boost pkg-config 
-brew uninstall qt
 brew uninstall qt5
-brew update
 brew install protobuf miniupnpc openssl qrencode berkeley-db4 
 # Might need these later: libevent librsvg
 curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/fdfc724dd532345f5c6cdf47dc43e99654e6a5fd/Formula/qt5.rb
