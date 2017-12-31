1bc82b21ed2ad6bf985d1baa939afc3604d115f3
Eric Kryski <e.kryski@gmail.com>
2017-12-31 01:22:47 -0700

    fix build scripts for OS X 10.12

--- building/mac/requirements.sh
+++ building/mac/requirements.sh
@@ -1,8 +1,4 @@
-#brew install boost pkg-config 
 brew uninstall qt5
-brew install protobuf miniupnpc openssl qrencode berkeley-db4 
-# Might need these later: libevent librsvg
-curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/fdfc724dd532345f5c6cdf47dc43e99654e6a5fd/Formula/qt5.rb
-brew install ./qt5.rb
-brew link --force qt5
+brew install boost pkg-config
+brew install qt@5.5 protobuf miniupnpc openssl qrencode berkeley-db4 automake
 
