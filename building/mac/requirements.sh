5538bee5804d7b7646cf5979bacd280e60321c83
Varun Villait <vvillait88@yahoo.com>
2017-12-30 23:50:55 -0800

    test fix for mac build

--- building/mac/requirements.sh
+++ building/mac/requirements.sh
@@ -1,11 +1,14 @@
-# Remove any old qt5 lingering around
+#brew install boost pkg-config
 brew uninstall qt5
-
-# Make sure we have the C libs we need
-brew install boost pkg-config automake
-
-# Install some deps
-brew install qt@5.5 protobuf miniupnpc openssl qrencode berkeley-db4
-
-# Make sure our stuff is linked in our path
-brew link automake autoconf
+brew install protobuf miniupnpc openssl qrencode
+brew install https://raw.github.com/mxcl/homebrew/master/Library/Formula/berkeley-db4.rb -–without-java
+cd ..
+db-4.8.30/dist/configure --prefix=/usr/local/Cellar/berkeley-db4/4.8.30 --mandir=/usr/local/Cellar/berkeley-db4/4.8.30/share/man --enable-cxx
+make
+make install
+exit
+brew link berkeley-db4 --force
+# Might need these later: libevent librsvg
+curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/fdfc724dd532345f5c6cdf47dc43e99654e6a5fd/Formula/qt5.rb
+brew install ./qt5.rb
+brew link --force qt5
