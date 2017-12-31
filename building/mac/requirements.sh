cb7b9d9baeaaa8498cdc9ee10059181a06bfac12
Robert McDonald <BobbyMcWho@users.noreply.github.com>
2017-12-31 00:09:02 -0500

    Fix mac build failure in CI
    Adds brew update prior to brew install to avoid Ruby versioning error

--- building/mac/requirements.sh
+++ building/mac/requirements.sh
@@ -1,9 +1,11 @@
 #brew install boost pkg-config 
 brew uninstall qt5
+brew update # Workaround for Travis CI issue on Apple builds
 brew install protobuf miniupnpc openssl qrencode berkeley-db4
 brew link berkeley-db4 --force
 # Might need these later: libevent librsvg
 curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/fdfc724dd532345f5c6cdf47dc43e99654e6a5fd/Formula/qt5.rb
+brew update # Workaround for Travis CI issue on Apple builds
 brew install ./qt5.rb
-brew link --force qt5
+brew link qt5 --force
 
