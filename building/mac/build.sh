06d204f729c39e5bf41d35e0033672466cc27039
Joe Hanink <jhanink@gmail.com>
2017-12-31 09:49:14 -0800

    fix paths

--- building/mac/build.sh
+++ building/mac/build.sh
@@ -1,8 +1,8 @@
 
-export PKG_CONFIG_PATH=/usr/local/opt/qt@5.10.0/lib/pkgconfig
-export PATH=/usr/local/opt/qt@5.10.0/bin:$PATH
-export QT_CFLAGS="-I/usr/local/opt/qt@5.10.0/lib/QtWebKitWidgets.framework/Versions/5/Headers -I/usr/local/opt/qt@5.10.0/lib/QtDBus.framework/Versions/5/Headers -I/usr/local/opt/qt@5.10.0/lib/QtWidgets.framework/Versions/5/Headers -I/usr/local/opt/qt@5.10.0/lib/QtWebKit.framework/Versions/5/Headers -I/usr/local/opt/qt@5.10.0/lib/QtNetwork.framework/Versions/5/Headers -I/usr/local/opt/qt@5.10.0/lib/QtGui.framework/Versions/5/Headers -I/usr/local/opt/qt@5.10.0/lib/QtCore.framework/Versions/5/Headers -I. -I/usr/local/opt/qt@5.10.0/mkspecs/macx-clang -F/usr/local/opt/qt@5.10.0/lib"
-export QT_LIBS="-F/usr/local/opt/qt@5.10.0/lib -framework QtWidgets -framework QtGui -framework QtCore -framework DiskArbitration -framework IOKit -framework OpenGL -framework AGL -framework QtNetwork -framework QtWebKit -framework QtWebKitWidgets -framework QtDBus"
+export PKG_CONFIG_PATH=/usr/local/opt/qt@5.10/lib/pkgconfig
+export PATH=/usr/local/opt/qt@5.10/bin:$PATH
+export QT_CFLAGS="-I/usr/local/opt/qt@5.10/lib/QtWebKitWidgets.framework/Versions/5/Headers -I/usr/local/opt/qt@5.10/lib/QtDBus.framework/Versions/5/Headers -I/usr/local/opt/qt@5.10/lib/QtWidgets.framework/Versions/5/Headers -I/usr/local/opt/qt@5.10/lib/QtWebKit.framework/Versions/5/Headers -I/usr/local/opt/qt@5.10/lib/QtNetwork.framework/Versions/5/Headers -I/usr/local/opt/qt@5.10/lib/QtGui.framework/Versions/5/Headers -I/usr/local/opt/qt@5.10/lib/QtCore.framework/Versions/5/Headers -I. -I/usr/local/opt/qt@5.10/mkspecs/macx-clang -F/usr/local/opt/qt@5.10/lib"
+export QT_LIBS="-F/usr/local/opt/qt@5.10/lib -framework QtWidgets -framework QtGui -framework QtCore -framework DiskArbitration -framework IOKit -framework OpenGL -framework AGL -framework QtNetwork -framework QtWebKit -framework QtWebKitWidgets -framework QtDBus"
 
 ./autogen.sh
 ./configure --with-gui=qt5
