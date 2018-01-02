f50c34fe3e85f03ca1c6c890ea799e4b773b7730
Michał Piątek <michpiatek@gmail.com>
2018-01-02 20:35:41 +0100

    more sane logic of key search in Unlock

--- src/keystore.cpp
+++ src/keystore.cpp
@@ -108,12 +108,15 @@ bool CCryptoKeyStore::Unlock(const CKeyingMaterial& vMasterKeyIn)
             key.SetPubKey(vchPubKey);
             key.SetSecret(vchSecret);
             if (key.GetPubKey() == vchPubKey)
-                break;
+            {
+                vMasterKey = vMasterKeyIn;
+                NotifyStatusChanged(this);
+                return true;
+            }
         }
-        vMasterKey = vMasterKeyIn;
     }
-    NotifyStatusChanged(this);
-    return true;
+    
+    return false;
 }
 
 bool CCryptoKeyStore::AddKey(const CKey& key)
