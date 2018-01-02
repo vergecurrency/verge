95ec4b1b79a81dac39aa95639a6833afdae61911
gukak <antoine.renard@gmail.com>
2018-01-02 17:07:04 +0100

    Update bitcoin_fr.ts

--- src/qt/locale/bitcoin_fr.ts
+++ src/qt/locale/bitcoin_fr.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en">
+<TS version="2.0" language="fr">
 <defaultcodec>UTF-8</defaultcodec>
 <context>
     <name>AboutDialog</name>
