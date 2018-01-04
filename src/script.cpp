f33986ab90ac7fa46d2de111824497eb3ac7bf34
Michał Piątek <michpiatek@gmail.com>
2018-01-04 06:57:06 +0100

    fixed stealth tx

--- src/script.cpp
+++ src/script.cpp
@@ -1327,11 +1327,12 @@ bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, vector<vector<unsi
                 // small pushdata, <= MAX_OP_RETURN_RELAY bytes
                 if (vch1.size() > MAX_OP_RETURN_RELAY)
                     break;
-	    }
-	    else if (opcode2 == OP_RETURN)
-	    {
-		break;
-	    }
+	        }
+            else if (opcode2 == OP_RETURN)
+            {
+                typeRet = TX_NULL_DATA;
+                return true;
+            }
             else if (opcode1 != opcode2 || vch1 != vch2)
             {
                 // Others must match exactly
