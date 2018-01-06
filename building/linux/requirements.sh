d3dc2d6e9edb264cbe4e06cae3d3dbfd47024b41
sunerok <justinvforvendetta@gmail.com>
2018-01-06 11:05:03 -0500

    Revert "fix qt deps"
    This reverts commit 40fd746fde91a8ce033042af8baa9c6186bc1559.

--- building/linux/requirements.sh
+++ building/linux/requirements.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa > /dev/null
 sudo add-apt-repository --yes ppa:bitcoin/bitcoin > /dev/null
 sudo apt-get update -qq > /dev/null
 
-sudo apt-get --yes -qq install libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev libsqlite3-dev zip > /dev/null
+sudo apt-get --yes -qq install qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev zip > /dev/null
 sudo apt-get --yes -qq install qt5-default qttools5-dev-tools libdb4.8-dev libdb4.8++-dev > /dev/null
 sudo apt-get --yes -qq install build-essential libseccomp-dev libcap-dev libtool autotools-dev automake pkg-config libssl-dev libevent-dev zlib1g-dev bsdmainutils git libboost-system-dev libboost-filesystem-dev libqrencode-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libboost-all-dev libprotobuf-dev protobuf-compiler libminiupnpc-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev > /dev/null
 
