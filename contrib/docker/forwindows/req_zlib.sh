133eb4bda991ba874971a90de27cb23a0c78c139
fradelo <35038000+fradelo@users.noreply.github.com>
2018-01-08 02:59:21 +0200

    Fixed  zlib source path

--- contrib/docker/forwindows/req_zlib.sh
+++ contrib/docker/forwindows/req_zlib.sh
@@ -1,7 +1,7 @@
 
 echo "=== Building ZLIB now..."
 cd /tmp
-wget http://zlib.net/zlib-1.2.8.tar.gz
+wget http://zlib.net/fossils/zlib-1.2.8.tar.gz
 tar xvfz zlib-1.2.8.tar.gz
 cd zlib-1.2.8
 sed -e s/"PREFIX ="/"PREFIX = i686-w64-mingw32.static-"/ -i win32/Makefile.gcc
