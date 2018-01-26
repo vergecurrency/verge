2be388cc813cc025d6b81a264502ab860f49ff9f
Michał Piątek <michpiatek@gmail.com>
2018-01-26 22:22:13 +0100

    Fix for #566

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -1216,6 +1216,8 @@ Array RPCConvertValues(const std::string &strMethod, const std::vector<std::stri
     if (strMethod == "createrawtransaction"   && n > 1) ConvertTo<Object>(params[1]);
     if (strMethod == "signrawtransaction"     && n > 1) ConvertTo<Array>(params[1], true);
     if (strMethod == "signrawtransaction"     && n > 2) ConvertTo<Array>(params[2], true);
+    if (strMethod == "scanforalltxns"         && n > 0) ConvertTo<boost::int64_t>(params[0]);
+    if (strMethod == "scanforstealthtxns"     && n > 0) ConvertTo<boost::int64_t>(params[0]);
 
     return params;
 }
