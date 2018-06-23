412a950f60adea12c0d64054381ecb2660647cb1
Jeff S <31370541+jrsikors@users.noreply.github.com>
2018-06-23 13:23:19 -0500

    Update bitcoin_es.ts

--- src/qt/locale/bitcoin_es.ts
+++ src/qt/locale/bitcoin_es.ts
@@ -1806,12 +1806,12 @@ del medio”.</translation>
     </message>
     <message>
         <location line="+3"/>
-        <source>Generated coins must mature 25 blocks before they can be spent. When you generated this block, it was broadcast to the network to be added to the 
+        <source>Generated coins must mature 140 blocks before they can be spent. When you generated this block, it was broadcast to the network to be added to the 
 
 block chain. If it fails to get into the chain, its state will change to &quot;not accepted&quot; and it won&apos;t be spendable. This may occasionally happen if 
 
 another node generates a block within a few seconds of yours.</source>
-        <translation>Las monedas generadas deben madurar durante 25 bloques antes de que puedan ser gastadas. Cuando generaste este bloque, fue emitido a la red 
+        <translation>Las monedas generadas deben madurar durante 140 bloques antes de que puedan ser gastadas. Cuando generaste este bloque, fue emitido a la red 
 
 para ser añadido a la cadena de bloques. Si no logra ingresar a la cadena, su estado cambiará a  &quot;no aceptado&quot; y no será gastable. Estoy puede ocurrir 
 
