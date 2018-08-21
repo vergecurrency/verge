1c84f38a7790ec86b3a8aa980b3ea927a3c3414f
malt0se <39808902+malt0se@users.noreply.github.com>
2018-08-21 17:32:52 +0300

    Update go.sh (#799)
    corrects BerkeleyDB-4.8's "dbinc/atomic.h" to get rid of the "atomic.h.. warning: conflicting types for built-in function" error during compilation.
    
    more thorough BerkeleyDB-4.8 check
    
    removes unnecessary stuff, i.e. blockchain zip storing, which was very bad idea

--- go.sh
+++ go.sh
@@ -5,14 +5,13 @@ VERGEPWD=$PWD
 DISTR=$(lsb_release -si)
 IFSWAP=$(cat /proc/swaps | wc -l)
 #If zips location is changed, just modify these links accordingly
-VERGE_BLOCKCHAIN_ZIP_00_LOCATION="https://verge-blockchain.com"
-VERGE_BLOCKCHAIN_ZIP_01_LOCATION="https://s1.verge-blockchain.com"
-VERGE_BLOCKCHAIN_ZIP_00="https://verge-blockchain*.*zip"
-VERGE_BLOCKCHAIN_ZIP_01="https://s1.verge-blockchain*.*zip"
-VERGE_REGEX="Verge-Blockchain"
+VERGE_BLOCKCHAIN_ZIP_LOCATION_00="https://verge-blockchain.com"
+VERGE_BLOCKCHAIN_ZIP_LOCATION_01="https://s1.verge-blockchain.com"
+VERGE_BLOCKCHAIN_ZIP_REGEXP_00="https://verge-blockchain*.*zip"
+VERGE_BLOCKCHAIN_ZIP_REGEXP_01="https://s1.verge-blockchain*.*zip"
 #If torrent location is changed, just modify these links accordingly
 VERGE_BLOCKCHAIN_TORRENT_LOCATION="https://verge-blockchain.com/torrent1"
-VERGE_BLOCKCHAIN_TORRENT_00="https://verge-blockchain*.*zip.torrent"
+VERGE_BLOCKCHAIN_TORRENT_REGEXP="https://verge-blockchain*.*zip.torrent"
 
 <<EOF 
 	If you want to ensure "unpredictability at system startup" (provided that start-ups do not 
@@ -47,20 +46,19 @@ if [ $IFSWAP -gt 1 ]; then
 printf "\nSwap exists.\n"
 else
 	printf "\nAdding temporary Swap...\n"
-	sudo dd if=/dev/zero of=/swapfile bs=1M count=1024
-	sudo chmod 600 /swapfile
-	sudo mkswap /swapfile
-	sudo swapon /swapfile
+	sudo dd if=/dev/zero of=/temp_swap bs=1M count=1024
+	sudo chmod 600 /temp_swap
+	sudo mkswap /temp_swap
+	sudo swapon /temp_swap
 fi
 
 # Install dependencies
-# Also, feel free to append "add-bitcoin-repository-COMMAND-LIST" for others distributions
 sudo apt-get -y install software-properties-common
 case "$DISTR" in
 	Debian)
 		# https://launchpad.net/~bitcoin/+archive/ubuntu/bitcoin
 		# No longer supports precise, due to its ancient gcc and Boost versions.
-		sudo apt-get install dirmngr
+		sudo apt-get -y install dirmngr
 		sudo add-apt-repository "deb http://ppa.launchpad.net/bitcoin/bitcoin/ubuntu trusty main"
 		sudo apt-get update >> /dev/null 2> /tmp/temp_apt_key.txt
 		TEMP_APT_KEY=$(cat /tmp/temp_apt_key.txt | cut -d":" -f6 | cut -d" " -f3)
@@ -72,7 +70,7 @@ case "$DISTR" in
 		;;
 esac
 
-sudo apt-get update && sudo apt-get -y install git libcanberra-gtk-module lynx xdg-utils unzip libdb4.8-dev libdb4.8++-dev build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git libboost-all-dev libminiupnpc-dev libqt5gui5 libqt5core5a libqt5dbus5 libevent-dev qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev libseccomp-dev libcap-dev
+sudo apt-get update && sudo apt-get -y install git libcanberra-gtk-module lynx xdg-utils unzip libdb4.8-dev libdb4.8++-dev build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git libboost-all-dev libminiupnpc-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev libseccomp-dev libcap-dev
 rc=$?
 
 if [ $rc != 0 ]; then
@@ -80,25 +78,25 @@ if [ $rc != 0 ]; then
 	sleep 2
 fi
 
-# Check if boost (>= 1.63 version) has been already installed
 printf "\nChecking boost...\n"
 BOOST_VERSION=$(grep --include=*.hpp -r '/usr/include' -e "define BOOST_LIB_VERSION" | sed 's/[^0-9]//g')
-BOOST_LIB=$(find /usr/lib -name libboost_chrono.so | wc -l)
-if [ $BOOST_LIB -gt 0 ] && [ $BOOST_VERSION -ge 163 ]; then
+BOOST_LIB=$(find /usr/lib -name libboost_chrono.so)
+if [ ! -z "$BOOST_LIB" ] && [ $BOOST_VERSION -ge 163 ]; then
 	BOOST_ARG="/usr/lib/"
 	printf "\nWill use installed boost\n"
 	sleep 2
 else
+	BOOST_ARG="/usr/local/lib/"
 	# Check if boost (>= 1.63 version) has been already compiled
 	BOOST_VERSION=$(grep --include=*.hpp -r '/usr/local/include' -e "define BOOST_LIB_VERSION" | sed 's/[^0-9]//g')
-	BOOST_LIB=$(find /usr/local -name libboost_chrono.so | wc -l)
-	if [ $BOOST_LIB -gt 0 ] && [ $BOOST_VERSION -ge 163 ]; then
+	BOOST_LIB=$(find /usr/local/lib -name libboost_chrono.so)
+	if [ ! -z "$BOOST_LIB" ] && [ $BOOST_VERSION -ge 163 ]; then
 		printf "\nWill use compiled boost\n"
 		sleep 2
 	else
 		# Check if boost_1_63_0.zip has been already downloaded
 		BOOST=$(find $HOME -name boost_1_63_0.zip | head -n 1)
-		if [ -z $BOOST ]; then
+		if [ -z "$BOOST" ]; then
 			wget -O $HOME/boost_1_63_0.zip https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.zip
 			rc=$?
 			if [ $rc != 0 ]; then
@@ -111,66 +109,66 @@ else
 		fi
 		unzip $BOOST
 		cd boost_1_63_0
-		sh bootstrap.sh
+		./bootstrap.sh
 		sudo ./b2 install
-		sudo ldconfig
 		cd $VERGEPWD
 		rm -rf boost_1_63_0
 	fi
-	BOOST_ARG="/usr/local/lib/"
 fi
 
-# Check if BerkeleyDb 4.8 has been already installed
 printf "\nChecking BerkeleyDB...\n"
-BERKELEY_LIB=$(find /usr/lib -name libdb_cxx-4.8.so | wc -l)
-if [ $BERKELEY_LIB -gt 0 ]; then
-	BERKELEY_ARG="/usr"
-	printf "\nWill use installed BerkeleyDB-4.8\n"
-	sleep 2
-else
-	# Check if BerkeleyDb 4.8 has been already compiled
-	BERKELEY_LIB=$(find /usr/local/BerkeleyDB.4.8/lib/ -name libdb_cxx-4.8.so | wc -l)
-	if [ $BERKELEY_LIB -gt 0 ]; then
-		printf "\nWill use compiled BerkeleyDB-4.8\n"
+BERKELEY_LIB=$(find /usr/lib -name libdb_cxx-4.8.so)
+if [ -e $BERKELEY_LIB ]; then
+	if [ ! -L $BERKELEY_LIB ]; then
+		# BerkeleyDB-4.8 has been successfully installed from Bitcoin repo's
+		BERKELEY_ARG="/usr"
+		printf "\nWill use installed BerkeleyDB-4.8\n"
 		sleep 2
 	else
-		# Check if berkeley-db-4.8.30.NC.tar.gz has been already downloaded
-		BERKELEYDB=$(find $HOME -name db-4.8.30.NC.tar.gz | head -n 1)
-		if [ -z $BERKELEYDB ]; then
-			wget -O $HOME/db-4.8.30.NC.tar.gz https://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
-			rc=$?
-			if [ $rc != 0 ]; then
-				printf "\nFailed to get BerkeleyDB-4.8 from https://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz\n"
-				exit $rc
-			fi
-			printf "\nWill compile BerkeleyDB-4.8...\n"
-			sleep 2
-			BERKELEYDB=$HOME/db-4.8.30.NC.tar.gz
-		fi
-		tar -xzvf $BERKELEYDB
-		cd db-4.8.30.NC/build_unix
-		../dist/configure --enable-cxx
-		make
-		sudo make install
-		cd $VERGEPWD
-		rm -rf db-4.8.30.NC
+		# BerkeleyDB-4.8 has been successfully compiled earlier
+		BERKELEY_ARG="/usr/local/BerkeleyDB.4.8"
+		printf "\nWill use compiled BerkeleyDB-4.8\n"
+		sleep 2
 	fi
+else
+	# There is no BerkeleyDB-4.8 compiled or installed
 	BERKELEY_ARG="/usr/local/BerkeleyDB.4.8"
+	printf "\nWill compile BerkeleyDB-4.8...\n"
+	sleep 2
+	# Check if berkeley-db-4.8.30.NC.tar.gz has been already downloaded
+	BERKELEYDB=$(find $HOME -name db-4.8.30.NC.tar.gz | head -n 1)
+	if [ -z "$BERKELEYDB" ]; then
+		wget -O $HOME/db-4.8.30.NC.tar.gz https://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+		rc=$?
+		if [ $rc != 0 ]; then
+			printf "\nFailed to get BerkeleyDB-4.8 from https://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz\n"
+			exit $rc
+		fi
+		sleep 2
+		BERKELEYDB=$HOME/db-4.8.30.NC.tar.gz
+	fi
+	tar -xzvf $BERKELEYDB
+	# Fixing "atomic.h.. warning: conflicting types for built-in function" error during compilation
+	sudo sed -i 's/__atomic_compare_exchange(/__atomic_compare_exchange_db(/g' db-4.8.30.NC/dbinc/atomic.h
+	cd db-4.8.30.NC/build_unix
+	../dist/configure --enable-cxx
+	make
+	sudo make install
+	sudo ln -sf /usr/local/BerkeleyDB.4.8/lib/libdb-4.8.so /usr/lib/libdb-4.8.so
+	sudo ln -sf /usr/local/BerkeleyDB.4.8/lib/libdb_cxx-4.8.so /usr/lib/libdb_cxx-4.8.so
+	cd $VERGEPWD
+	rm -rf db-4.8.30.NC
 fi
 
 printf "\n"
 ./autogen.sh
-#chmod 777 ~/VERGE/share/genbuild.sh
-#chmod 777 ~/VERGE/src/leveldb/build_detect_platform
 ./configure CPPFLAGS="-I$BERKELEY_ARG/include -O2" LDFLAGS="-L$BERKELEY_ARG/lib" --with-gui=qt5 --with-boost-libdir=$BOOST_ARG
 
-#https://github.com/google/leveldb/issues/556
-#make -j$(nproc)
 make
 rc=$?
-if [ -e /swapfile ]; then
-	sudo swapoff /swapfile
-	sudo rm -f /swapfile
+if [ -e /temp_swap ]; then
+	sudo swapoff /temp_swap
+	sudo rm -f /temp_swap
 fi
 
 if [ $rc != 0 ]; then
@@ -181,17 +179,15 @@ if [ $rc != 0 ]; then
 else
 	printf "\nCompilation succeeded, now installing...\n"
 	sleep 2
-#	sudo strip src/VERGEd
-#	sudo strip src/qt/VERGE-qt
 	sudo make install
 	# Create Menu icon
-	printf "[Desktop Entry]\nType=Application\nName=VERGE\nGenericName=VERGE Qt Wallet\nIcon=/usr/share/icons/verge.png\nCategories=Network;Internet;\nExec=VERGE-qt\nTerminal=false" > $HOME/Desktop/VERGE.desktop
+	printf "[Desktop Entry]\nType=Application\nName=VERGE\nGenericName=VERGE Qt Wallet\nIcon=/usr/share/icons/verge.png\nCategories=Network;Internet;\nExec=VERGE-qt\nTerminal=false" > $HOME/VERGE/VERGE.desktop
 	sudo cp -f src/qt/res/icons/verge.png /usr/share/icons/
-	sudo cp -f $HOME/Desktop/VERGE.desktop /usr/share/applications/
+	sudo cp -f $HOME/VERGE/VERGE.desktop /usr/share/applications/
 	#// Create the config file with random user and password
 	mkdir -p $HOME/.VERGE
 	if [ -e $HOME/.VERGE/VERGE.conf ]; then
-		cp -af $HOME/.VERGE/VERGE.conf $HOME/.VERGE/VERGE.bak
+		cp -af $HOME/.VERGE/VERGE.conf $HOME/.VERGE/VERGE.conf.back
 	fi
 	printf "rpcuser=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 26)\nrpcpassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 26)\nrpcport=20102\nport=21102\ndaemon=1\nlisten=1\nserver=1\n" > $HOME/.VERGE/VERGE.conf
 #	printf "\naddnode=103.18.40.125:21102\naddnode=104.131.144.82:21102\naddnode=138.197.68.130:21102\naddnode=144.76.167.66:21102\naddnode=152.186.36.86:21102\naddnode=159.203.121.202:21102\naddnode=172.104.157.38:21102\naddnode=192.99.7.127:21102\naddnode=219.89.84.46:21102\n	addnode=45.32.129.168:21102\naddnode=45.55.59.206:21102\naddnode=46.4.64.68:21102\naddnode=51.15.46.1:21102\naddnode=52.9.109.214:21102\naddnode=66.55.64.183:21102\naddnode=67.167.207.164:21102\naddnode=78.46.190.152:21102\n">> $HOME/.VERGE/VERGE.conf
@@ -202,41 +198,41 @@ else
 		printf "\n2 - torrent file\n\n"
 		read CONFIRM
 		if [ "$CONFIRM" = "1" ]; then
-			# To ensure that no "go-sh"-named archives remained
-			rm -f $HOME/.VERGE/go.sh-$VERGE_REGEX*.zip
 			# Temporarily ignore all notifications about self-signed/letsencrypt certificates for lynx. But better solution would be to fix the certificate problem.
 			printf "FORCE_SSL_PROMPT:yes\n">lynx.cfg
-			VERGE_BLOCKCHAIN_ZIP_NAME_OLD=$(find $HOME/.VERGE -name *$VERGE_REGEX*.zip | sed 's/.*\///g')
-			VERGE_BLOCKCHAIN_ZIP_NAME_NEW_00=$(lynx -cfg=lynx.cfg -dump -listonly $VERGE_BLOCKCHAIN_ZIP_00_LOCATION | grep -o $VERGE_BLOCKCHAIN_ZIP_00 | sed 's/.*\///g')
-			if [ -z "$VERGE_BLOCKCHAIN_ZIP_NAME_NEW_00" ]; then
-				printf "\nCan't download VERGE Blockchain zip from $VERGE_BLOCKCHAIN_ZIP_00_LOCATION, will try $VERGE_BLOCKCHAIN_ZIP_01_LOCATION\n"
-				VERGE_BLOCKCHAIN_ZIP_NAME_NEW_01=$(lynx -cfg=lynx.cfg -dump -listonly $VERGE_BLOCKCHAIN_ZIP_01_LOCATION | grep -o $VERGE_BLOCKCHAIN_ZIP_01 | sed 's/.*\///g')
-				if [ -z "$VERGE_BLOCKCHAIN_ZIP_NAME_NEW_01" ]; then
-					printf "\nCan't download VERGE Blockchain zip from $VERGE_BLOCKCHAIN_ZIP_01_LOCATION\n"
+			VERGE_BLOCKCHAIN_ZIP_NAME_00=$(lynx -cfg=lynx.cfg -dump -listonly $VERGE_BLOCKCHAIN_ZIP_LOCATION_00 | grep -o $VERGE_BLOCKCHAIN_ZIP_REGEXP_00 | sed 's/.*\///g')
+			if [ -z "$VERGE_BLOCKCHAIN_ZIP_NAME_00" ]; then
+				printf "\nCan't download VERGE Blockchain zip from $VERGE_BLOCKCHAIN_ZIP_LOCATION_00, will try $VERGE_BLOCKCHAIN_ZIP_LOCATION_01\n"
+				VERGE_BLOCKCHAIN_ZIP_NAME_01=$(lynx -cfg=lynx.cfg -dump -listonly $VERGE_BLOCKCHAIN_ZIP_LOCATION_01 | grep -o $VERGE_BLOCKCHAIN_ZIP_REGEXP_01 | sed 's/.*\///g')
+				if [ -z "$VERGE_BLOCKCHAIN_ZIP_NAME_01" ]; then
+					printf "\nCan't download VERGE Blockchain zip from $VERGE_BLOCKCHAIN_ZIP_LOCATION_01\n"
 				else
-					if [ ! -z "$VERGE_BLOCKCHAIN_ZIP_NAME_OLD" ] && [ "$VERGE_BLOCKCHAIN_ZIP_NAME_OLD" != "$VERGE_BLOCKCHAIN_ZIP_NAME_NEW_01" ]; then
-						rm -f $HOME/.VERGE/$VERGE_BLOCKCHAIN_ZIP_NAME_OLD
-					fi
-					wget -c --no-check-certificate -O $HOME/.VERGE/$VERGE_BLOCKCHAIN_ZIP_NAME_NEW_01 $(lynx -cfg=lynx.cfg -dump -listonly $VERGE_BLOCKCHAIN_ZIP_01_LOCATION | grep -o $VERGE_BLOCKCHAIN_ZIP_01)
+					CURRENT_BLOCKCHAIN_ZIP_NAME=$VERGE_BLOCKCHAIN_ZIP_NAME_01
+					CURRENT_BLOCKCHAIN_ZIP_LOCATION=$VERGE_BLOCKCHAIN_ZIP_LOCATION_01
+					CURRENT_BLOCKCHAIN_ZIP_REGEXP=$VERGE_BLOCKCHAIN_ZIP_REGEXP_01
 				fi
 			else
-				if [ ! -z "$VERGE_BLOCKCHAIN_ZIP_NAME_OLD" ] && [ "$VERGE_BLOCKCHAIN_ZIP_NAME_OLD" != "$VERGE_BLOCKCHAIN_ZIP_NAME_NEW_00" ]; then
-					rm -f $HOME/.VERGE/$VERGE_BLOCKCHAIN_ZIP_NAME_OLD
-				fi
-				wget -c --no-check-certificate -O $HOME/.VERGE/$VERGE_BLOCKCHAIN_ZIP_NAME_NEW_00 $(lynx -cfg=lynx.cfg -dump -listonly $VERGE_BLOCKCHAIN_ZIP_00_LOCATION | grep -o $VERGE_BLOCKCHAIN_ZIP_00)
+				CURRENT_BLOCKCHAIN_ZIP_NAME=$VERGE_BLOCKCHAIN_ZIP_NAME_00
+				CURRENT_BLOCKCHAIN_ZIP_LOCATION=$VERGE_BLOCKCHAIN_ZIP_LOCATION_00
+				CURRENT_BLOCKCHAIN_ZIP_REGEXP=$VERGE_BLOCKCHAIN_ZIP_REGEXP_00
 			fi
-			if [ -e $HOME/.VERGE/*$VERGE_REGEX*.zip ]; then
-				unzip $HOME/.VERGE/*$VERGE_REGEX*.zip -d $HOME/.VERGE
+			if [ ! -z "$CURRENT_BLOCKCHAIN_ZIP_NAME" ]; then
+				wget -c --no-check-certificate -O $HOME/.VERGE/$CURRENT_BLOCKCHAIN_ZIP_NAME $(lynx -cfg=lynx.cfg -dump -listonly $CURRENT_BLOCKCHAIN_ZIP_LOCATION | grep -o $CURRENT_BLOCKCHAIN_ZIP_REGEXP)
+			fi
+			if [ -e $HOME/.VERGE/$CURRENT_BLOCKCHAIN_ZIP_NAME ]; then
+				unzip $HOME/.VERGE/$CURRENT_BLOCKCHAIN_ZIP_NAME -d $HOME/.VERGE
 				rc=$?
 				if [ $rc != 0 ]; then
-					printf "\nVERGE Blockchain zip download has not been completed\n"
+					printf "\n$CURRENT_BLOCKCHAIN_ZIP_NAME download has not been completed\n"
+				else
+					rm -f $HOME/.VERGE/$CURRENT_BLOCKCHAIN_ZIP_NAME
 				fi
 			else
-				printf "\nVERGE Blockchain zip download was not successful\n"
+				printf "\n$CURRENT_BLOCKCHAIN_ZIP_NAME download was not successful\n"
 			fi
 		fi
 		if [ "$CONFIRM" = "2" ]; then
-			xdg-open 2>/dev/null 1>&2 $(lynx -cfg=lynx.cfg -dump -listonly $VERGE_BLOCKCHAIN_TORRENT_LOCATION | grep -o $VERGE_BLOCKCHAIN_TORRENT_00)
+			xdg-open 2>/dev/null 1>&2 $(lynx -cfg=lynx.cfg -dump -listonly $VERGE_BLOCKCHAIN_TORRENT_LOCATION | grep -o $VERGE_BLOCKCHAIN_TORRENT_REGEXP)
 			rc=$?
 			if [ $rc != 0 ]; then
 				printf "\nCan't download VERGE Blockchain torrent\n"
