813e0e1a047b6ff2da966b4d5686ff4780f3056d
marpme <marvin.piekarek5@gmail.com>
2018-10-22 21:10:47 +0200

    Adding moc as a requirement

--- building/mac/requirements.sh
+++ building/mac/requirements.sh
@@ -3,7 +3,6 @@ echo 'REMOVE OLD BREW COMPONENTS'
 brew uninstall qt5
 
 echo 'ADD REQUIRED BREW COMPONENTS'
-
 # Make sure we have the C libs we need
 brew install boost@1.60 pkg-config automake
 
@@ -15,7 +14,8 @@ echo 'ADD ADDITIONAL BREW COMPONENTS'
 # Install even more deps
 brew install protobuf miniupnpc openssl qrencode berkeley-db4
 
-echo 'LINK BREW COMPONENTS'
+brew install moc
 
+echo 'LINK BREW COMPONENTS'
 # Make sure our stuff is linked in our path
 brew link automake autoconf
