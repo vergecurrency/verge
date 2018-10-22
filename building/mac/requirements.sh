d7442ed5364ba48e166ce4e84e002c47befa28f1
marpme <marvin.piekarek5@gmail.com>
2018-10-22 10:46:21 +0200

    Travis fixes, remove bins & add logs

--- building/mac/requirements.sh
+++ building/mac/requirements.sh
@@ -1,11 +1,18 @@
+echo 'REMOVE OLD BREW COMPONENTS'
 #brew install boost pkg-config
 brew uninstall qt5
+
+echo 'ADD REQUIRED BREW COMPONENTS'
 
 # Make sure we have the C libs we need
 brew install boost@1.60 pkg-config automake
 
+echo 'ADD ADDITIONAL BREW COMPONENTS'
+
 # Install some deps
 brew install qt@5.5 protobuf miniupnpc openssl qrencode berkeley-db4
+
+echo 'LINK BREW COMPONENTS'
 
 # Make sure our stuff is linked in our path
 brew link automake autoconf
