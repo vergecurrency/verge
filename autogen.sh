b05057a6df5c8c779ff9cae6d5dae0aef76fe2f4
sunerok <justinvforvendetta@users.noreply.github.com>
2019-01-18 05:56:51 -0500

    update year

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Copyright (c) 2013-2016 The Bitcoin Core developers
+# Copyright (c) 2019 The Verge Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
