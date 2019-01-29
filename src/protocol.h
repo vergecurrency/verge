fffe237c0ff50cffdb2a1d5a33146a5db8fffeac
Marvin Piekarek <marpme@users.noreply.github.com>
2019-01-29 21:48:11 +0100

    Switch protocol ids for supported actions for node to support olders nodes too (#862)

--- src/protocol.h
+++ src/protocol.h
@@ -255,13 +255,13 @@ enum ServiceFlags : uint64_t {
     // VERGE Core nodes used to support this by default, without advertising this bit,
     // but no longer do as of protocol version 70011 (= NO_BLOOM_VERSION)
     NODE_BLOOM = (1 << 1),
+    // NODE_WITNESS indicates that a node can be asked for blocks and transactions including
+    // witness data.
+    NODE_WITNESS = (1 << 2),
     // NODE_GETUTXO means the node is capable of responding to the getutxo protocol request.
     // VERGE Core does not support this but a patch set called VERGE XT does.
     // See BIP 64 for details on how this is implemented.
-    NODE_GETUTXO = (1 << 2),
-    // NODE_WITNESS indicates that a node can be asked for blocks and transactions including
-    // witness data.
-    NODE_WITNESS = (1 << 3),
+    NODE_GETUTXO = (1 << 3),
     // NODE_XTHIN means the node supports Xtreme Thinblocks
     // If this is turned off then the node will not service nor make xthin requests
     NODE_XTHIN = (1 << 4),
