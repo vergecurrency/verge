4233517c6e14e26f08fc86c0c9a539ac42acf9c3
marpme <marpme@users.noreply.github.com>
2019-01-29 21:23:34 +0100

    Improve thread handling for tor

--- src/torcontroller.h
+++ src/torcontroller.h
@@ -9,45 +9,18 @@
 #define VERGE_TORCONTROLLER_H
 
 #include <stdint.h>
-
-#include <string>
-#include <cstring>
-#include <openssl/crypto.h>
-#include <openssl/ssl.h>
-#include <addrdb.h>
-#include <addrman.h>
-#include <amount.h>
-#include <bloom.h>
-#include <compat.h>
-#include <crypto/siphash.h>
-#include <hash.h>
-#include <limitedmap.h>
-#include <netaddress.h>
-#include <policy/feerate.h>
-#include <protocol.h>
-#include <random.h>
-#include <streams.h>
-#include <sync.h>
-#include <uint256.h>
-#include <threadinterrupt.h>
-#include <openssl/crypto.h>
-
 #include <atomic>
 #include <deque>
-#include <stdint.h>
 #include <thread>
 #include <memory>
-#include <condition_variable>
-#include <chainparams.h>
-#include <clientversion.h>
-#include <consensus/consensus.h>
-#include <crypto/common.h>
-#include <crypto/sha256.h>
-#include <primitives/transaction.h>
-#include <netbase.h>
+#include <string> 
+#include <iostream>
+
+#include <boost/filesystem.hpp>
+#include <boost/thread.hpp>
+#include <util/system.h>
+#include <threadinterrupt.h>
 #include <scheduler.h>
-#include <ui_interface.h>
-#include <util/strencodings.h>
 
 /** Default Port to run tor entry node on **/
 static const unsigned int DEFAULT_TOR_PORT = 9089;
@@ -56,7 +29,22 @@ static const unsigned int DEFAULT_TOR_PORT = 9089;
 static const unsigned int DEFAULT_TOR_CONTROL_PORT = 9051;
 
 char *convert_str(const std::string &s);
+
 void run_tor();
-void StartTor();
+
+/**
+ * Initializes a new tor thread within a new thread 
+ **/
+void InitalizeTorThread();
+
+/**
+ * Stops the known thread and tries to kill it
+ **/
+void StopTorController();
+
+/**
+ * Internally starts within the new thread
+ **/
+void StartTorController();
 
 #endif /* VERGE_TORCONTROLLER_H */
