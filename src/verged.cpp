7c4305d72262e98602d6cb210acb8288157f4e98
sunerok <justinvforvendetta@users.noreply.github.com>
2019-02-20 22:47:18 -0500

    add chain to interfaces

--- src/verged.cpp
+++ src/verged.cpp
@@ -12,6 +12,7 @@
 #include <clientversion.h>
 #include <compat.h>
 #include <fs.h>
+#include <interfaces/chain.h>
 #include <rpc/server.h>
 #include <init.h>
 #include <noui.h>
@@ -57,6 +58,9 @@ static void WaitForShutdown()
 //
 static bool AppInit(int argc, char* argv[])
 {
+	InitInterfaces interfaces;
+    interfaces.chain = interfaces::MakeChain();
+	
     bool fRet = false;
 
     //
@@ -165,7 +169,7 @@ static bool AppInit(int argc, char* argv[])
             // If locking the data directory failed, exit immediately
             return false;
         }
-        fRet = AppInitMain();
+        fRet = AppInitMain(interfaces);
     }
     catch (const std::exception& e) {
         PrintExceptionContinue(&e, "AppInit()");
@@ -179,7 +183,7 @@ static bool AppInit(int argc, char* argv[])
     } else {
         WaitForShutdown();
     }
-    Shutdown();
+    Shutdown(interfaces);
 
     return fRet;
 }
