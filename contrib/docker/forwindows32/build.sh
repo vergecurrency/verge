b592e88899e42f80e9431cd11134ba04a1a38d99
sunerok <justinvforvendetta@users.noreply.github.com>
2019-03-05 18:13:56 -0500

    seperate win32/64 docker

---
+++ contrib/docker/forwindows32/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# build the windows exe
+
+export LDFLAGS='-lssl -lcrypto -static -L/tmp/db-4.8.30.NC/build_windows -L/tmp/boost_1_55_0/stage/lib -L/tmp/miniupnpc-1.9.20160209 -L/tmp/protobuf-2.5.0/src/.libs/ -L/tmp/zlib-1.2.8' 
+export CFLAGS='-I/tmp/db-4.8.30.NC/build_windows -I/tmp/boost_1_55_0/boost -I/tmp -I/tmp/protobuf-2.5.0 -I/tmp/zlib-1.2.8'
+export CXXFLAGS=$CFLAGS
+export CPPFLAGS=$CFLAGS
+export BOOST_ROOT=/tmp/boost_1_55_0
+
+./autogen.sh --host=i686-w64-mingw32.static-
+
+./configure --host=i686-w64-mingw32.static 
+#./configure --host=i686-w64-mingw32.static --with-gui=qt5
+
+make check
+
+# For debugging purposes
+cat src/test-suite.log
+
+echo "done"
---
+++ contrib/docker/forwindows32/req_boost.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# boost
+echo "=== Building BOOST now..."
+cd /tmp
+wget 'http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.bz2/download' -O boost_1_58_0.tar.bz2
+tar jxf boost_1_58_0.tar.bz2
+cd /tmp/boost_1_58_0
+
+# (if troubleshooting and want to 'make clean all')
+# bjam --clean 
+
+./bootstrap.sh --without-icu  
+
+#echo "using gcc : 5.5.0 : i686-w64-mingw32.static-g++ : <rc>i686-w64-mingw32.static-windres <archiver>i686-w64-mingw32.static-ar ;" > user-config.jam
+
+echo "using gcc : 5.5.0 : i686-w64-mingw32-g++ : <rc>i686-w64-mingw32-windres <archiver>i686-w64-mingw32-ar ;" > user-config.jam
+
+./b2 -d 0 --user-config=user-config.jam toolset=gcc-mingw binary-format=pe target-os=windows release --without-python --without-wave --without-context --without-coroutine --without-mpi --without-graph --without-graph_parallel -sNO_BZIP2=1 threadapi=win32 threading=multi variant=release link=static runtime-link=static
+
+echo "=== done building BOOST =="
