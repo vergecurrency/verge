0bf937d2fc8bde897588e9e64888e9202e4e7889
marpme <marpme@users.noreply.github.com>
2019-03-08 16:26:37 +0100

    Downgrade libcap to 2.25 cause of existing bugs

--- depends/packages/libcap.mk
+++ depends/packages/libcap.mk
@@ -1,8 +1,8 @@
 package=libcap
-$(package)_version=2.26
+$(package)_version=2.25
 $(package)_download_path=https://git.kernel.org/pub/scm/libs/libcap/libcap.git/snapshot/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=94fa5a39f1a67a2c41bc01a895702cb3f46caef37dae7a6c8cceb1f5853afdcd
+$(package)_sha256_hash=be671ebdab7b5203be19cc76f2b4e29cbd962cfc4cf0030519fc39aee6db6d3d
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-static
