1686105b36af8e63abc1eca9085e2c74380cf1f6
marpme <marpme@users.noreply.github.com>
2019-03-09 21:09:32 +0100

    Disabling alternating compilers (some problem can happen)

--- .travis/test_05_before_script.sh
+++ .travis/test_05_before_script.sh
@@ -17,7 +17,7 @@ if [ -n "$OSX_SDK" -a -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; then
   tar -C depends/SDKs -xf depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz
 fi
 if [[ $HOST = *-mingw32 ]]; then
-  DOCKER_EXEC update-alternatives --set $HOST-g++ \$\(which $HOST-g++-posix\)
+  #DOCKER_EXEC update-alternatives --set $HOST-g++ \$\(which $HOST-g++-posix\)
 fi
 if [ -z "$NO_DEPENDS" ]; then
   DOCKER_EXEC CONFIG_SHELL= make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS
