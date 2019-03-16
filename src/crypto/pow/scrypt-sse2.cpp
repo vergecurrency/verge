92c11c49de8d4c32fdf01f77a4ede6e1b781baaa
Gilmanov Ildar <dev@ddwarf.org>
2019-03-16 18:01:09 +0700

    Fix building on Windows

--- src/crypto/pow/scrypt-sse2.cpp
+++ src/crypto/pow/scrypt-sse2.cpp
@@ -41,6 +41,8 @@
 #include <sys/endian.h>
 #endif
 
+#if defined(HAVE_SSE2)
+
 static inline void xor_salsa8_sse2(__m128i B[4], const __m128i Bx[4])
 {
 	__m128i X0, X1, X2, X3;
@@ -140,3 +142,5 @@ void scrypt_1024_1_1_256_sp_sse2(const char *input, char *output, char *scratchp
 
 	PBKDF2_SHA256((const uint8_t *)input, 80, B, 128, 1, (uint8_t *)output, 32);
 }
+
+#endif // #if defined(HAVE_SSE2)
