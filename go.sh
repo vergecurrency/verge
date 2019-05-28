8dc0af2aba9662f237b8592dcbae5d45c3729bad
badbrainIRC <egypte333@hotmail.com>
2019-05-28 16:51:33 -0400

    Update go.sh

--- go.sh
+++ go.sh
@@ -220,7 +220,15 @@ if [ -e ~/.VERGE/VERGE.conf ]
 then
     cp -a ~/.VERGE/VERGE.conf ~/.VERGE/VERGE.bak
 fi
-echo "rpcuser="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 26 ; echo '') '\n'"rpcpassword="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 26 ; echo '') '\n'"rpcport=20102" '\n'"port=21102" '\n'"daemon=1" '\n'"listen=1" '\n'"server=1" '\n'"deprecatedrpc=accounts" > ~/.VERGE/VERGE.conf
+rm ~/.VERGE/VERGE.conf
+echo "rpcuser="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 26) >> ~/.VERGE/VERGE.conf
+echo "rpcpassword="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 26) >> ~/.VERGE/VERGE.conf
+echo "rpcport=20102" >> ~/.VERGE/VERGE.conf
+echo "port=21102" >> ~/.VERGE/VERGE.conf
+echo "daemon=1" >> ~/.VERGE/VERGE.conf
+echo "listen=1" >> ~/.VERGE/VERGE.conf
+echo "server=1" >> ~/.VERGE/VERGE.conf
+"deprecatedrpc=accounts" >> ~/.VERGE/VERGE.conf
 
 #// Extract http link, download blockchain and install it.
 
