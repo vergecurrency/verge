a3ca2e3ad6d74bd3829eb8f940b59b6e030ef931
sunerok <justinvforvendetta@users.noreply.github.com>
2019-08-20 23:34:23 -0400

    base58

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -5,6 +5,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <amount.h>
+#include <base58.h>
 #include <chain.h>
 #include <consensus/validation.h>
 #include <core_io.h>
