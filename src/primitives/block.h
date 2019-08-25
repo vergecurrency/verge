637fb5f618259fdcac4a3343c03ca248b5165fe2
marpme <marpme@users.noreply.github.com>
2019-08-25 21:02:51 +0200

    Add a function to calculate the max time of the block-including transaction

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -304,12 +304,22 @@ public:
                     continue;
                 if(!key.Verify(GetHash(), vchBlockSig))
                     continue;
-
+                
                 return true;
             }
         }
         
         return false;
+    }
+
+    // ppcoin: get max transaction timestamp
+    int64_t GetMaxTransactionTime() const
+    {
+        int64_t maxTransactionTime = 0;
+        for(const auto& tx: vtx) {
+            maxTransactionTime = std::max(maxTransactionTime, (int64_t)tx->nTime);
+        }
+        return maxTransactionTime;
     }
     
     std::string ToString() const;
