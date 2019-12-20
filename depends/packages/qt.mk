ad6ac86f4b09b0ef67e89ee4e410e4778456fa5d
marpme <marpme@users.noreply.github.com>
2019-12-20 15:52:26 +0100

    Add QT platform

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -95,6 +95,7 @@ $(package)_config_opts_darwin += -device-option MAC_TARGET=$(host)
 $(package)_config_opts_darwin += -device-option MAC_LD64_VERSION=$(LD64_VERSION)
 endif
 
+$(package)_config_opts_linux = -xcb
 $(package)_config_opts_linux += -qt-xcb
 $(package)_config_opts_linux += -system-freetype
 $(package)_config_opts_linux += -no-feature-sessionmanager
