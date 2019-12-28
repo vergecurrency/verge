ef580dda0119bd03c97a7f47dd292ff9429f2b1d
marpme <marpme@pop-os.localdomain>
2019-12-28 17:08:21 +0100

    Add seeds without trying feeler connection

--- src/net.cpp
+++ src/net.cpp
@@ -1653,20 +1653,14 @@ void CConnman::ThreadDNSAddressSeed()
                 }
                 addrman.Add(vAdd, resolveSource);
             } else {
-                if(gArgs.GetBoolArg("-without-tor", false)) {
-                    LogPrintf("One Shot the seed: %s\n", seed);
-                    AddOneShot(seed);
-                } else {
-                    LogPrintf("Direct connect the seed: %s\n", seed);
-                    // We will directly connect to seeds that might be of interest for us :) 
-                    CNetAddr address;
-                    address.SetSpecial(seed);
+                // We will directly connect to seeds that might be of interest for us :) 
+                CNetAddr address;
+                address.SetSpecial(seed);
 
-                    CAddress onionAddress(CService(address, Params().GetDefaultPort()), requiredServiceBits);
-                    std::vector<CAddress> onionAddresses;
-                    onionAddresses.push_back(onionAddress);
-                    addrman.Add(onionAddresses, resolveSource);
-                }
+                CAddress onionAddress(CService(address, Params().GetDefaultPort()), requiredServiceBits);
+                std::vector<CAddress> onionAddresses;
+                onionAddresses.push_back(onionAddress);
+                addrman.Add(onionAddresses, resolveSource);
             }
         }
     }
