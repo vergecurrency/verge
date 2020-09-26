faf913ad4723664182ad64c7d51ce1443478b880
Patrick Julien <29350086+badbrainIRC@users.noreply.github.com>
2020-09-26 19:55:08 -0400

    Update go.sh
    fix grab the right file for bootstrap

--- go.sh
+++ go.sh
@@ -287,7 +287,7 @@ cd ~
 
 echo -n "Success....Blockchain is now downloading press Ctrl-C to cancel but it will take longer to sync from 0. And you will have to start verge manual"
 sudo rm QT-Wallet*.zip
-echo "wget --no-check-certificate " $(lynx --dump --listonly https://verge-blockchain.com/down/ | grep -o "https://verge-blockchain.com/blockchain5.*zip" | head -1 ) > link.sh
+echo "wget --no-check-certificate " $(lynx --dump --listonly https://verge-blockchain.com/download/ | grep -o "https://verge-blockchain*.*zip" | head -1 ) > link.sh
 sh link.sh
 unzip -o QT-Wallet*.zip -d ~/.VERGE
 sudo rm QT-Wallet*.zip
