d6188629d7675cd05dcca5af4f008a410ac7a67b
Mark Brooker <mark@walkjivefly.com>
2021-02-21 21:55:28 +0100

    Add -version option to verge-cli

--- src/verge-cli.cpp
+++ src/verge-cli.cpp
@@ -51,6 +51,8 @@ static void SetupCliArgs()
     gArgs.AddArg("-rpcwallet=<walletname>", "Send RPC for non-default wallet on RPC server (needs to exactly match corresponding -wallet option passed to verged)", false, OptionsCategory::OPTIONS);
     gArgs.AddArg("-stdin", "Read extra arguments from standard input, one per line until EOF/Ctrl-D (recommended for sensitive information such as passphrases).  When combined with -stdinrpcpass, the first line from standard input is used for the RPC password.", false, OptionsCategory::OPTIONS);
     gArgs.AddArg("-stdinrpcpass", strprintf("Read RPC password from standard input as a single line.  When combined with -stdin, the first line from standard input is used for the RPC password."), false, OptionsCategory::OPTIONS);
+    gArgs.AddArg("-version", "Print version and exit", false, OptionsCategory::OPTIONS);
+
 
     // Hidden
     gArgs.AddArg("-h", "", false, OptionsCategory::HIDDEN);
