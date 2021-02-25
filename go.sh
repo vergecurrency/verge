a5f0ecc6bedf872a668e170a3405016114cf0b3f
Patrick Julien <29350086+badbrainIRC@users.noreply.github.com>
2021-02-24 20:18:34 -0500

    Update go.sh
    take care of wallet.dat, backup and recopy when recompiling, now show openssl version as well.

--- go.sh
+++ go.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+#// full deployement :   wget -O - https://raw.githubusercontent.com/vergecurrency/VERGE/master/go.sh | bash
+var=`date +"%FORMAT_STRING"`
+now=`date +"%m_%d_%Y"`
+now=`date +"%Y-%m-%d-%s"`
 
-#// full deployement :   wget -O - https://raw.githubusercontent.com/badbrainIRC/VERGE/master/go.sh | bash
+if [ -e ~/.VERGE/VERGE.conf ]
+then
+    cp -a ~/.VERGE/VERGE.conf ~/VERGE.conf
+    cp -a ~/.VERGE/VERGE.conf ~/VERGE.conf${now}
+fi
+if [ -e ~/.VERGE/wallet.dat ]
+then
+    cp -a ~/.VERGE/wallet.dat ~/wallet.dat
+    cp -a ~/.VERGE/wallet.dat ~/Vwallet.dat${now}
+fi
+
 sudo rm -Rf ~/VERGE
 # generating entropy make it harder to guess the randomness!.
 echo "Initializing random number generator..."
@@ -165,6 +179,7 @@ else
      grep --include=*.hpp -r '/usr/' -e "define BOOST_LIB_VERSION"
 fi
 
+echo Libssl version: $(/usr/bin/openssl version)
 #// Clone files from repo, Permissions and make
 
 git clone --recurse-submodules https://github.com/vergecurrency/VERGE
@@ -277,6 +292,17 @@ echo "Categories=Network;Internet;" >> ~/Desktop/VERGE.desktop
 sudo chmod +x ~/Desktop/VERGE.desktop
 sudo cp ~/Desktop/VERGE.desktop /usr/share/applications/VERGE.desktop
 sudo chmod +x /usr/share/applications/VERGE.desktop
+
+if [ -e ~/VERGE.conf ]
+then
+    cp -a ~/VERGE.conf ~/.VERGE/VERGE.conf
+    rm ~/VERGE.conf
+fi
+if [ -e ~/wallet.dat ]
+then
+    cp -a ~/wallet.dat ~/.VERGE/wallet.dat
+    rm ~/wallet.dat
+fi
 
 # Erase all VERGE compilation directory , cleaning
 
