cc5711be0a978bcdc06c62569b129fe6ab4e4d9f
swat69 <swat69@users.noreply.github.com>
2022-01-31 18:04:06 +0100

    change path

--- go.sh
+++ go.sh
@@ -323,7 +323,7 @@ cd ~
 
 echo -n "Success....Blockchain is now downloading press Ctrl-C to cancel but it will take longer to sync from 0. And you will have to start verge manual"
 sudo rm QT-Wallet*.zip
-echo "wget --no-check-certificate " $(lynx --dump --listonly https://verge-blockchain.com/download/ | grep -o "https://verge-blockchain*.*zip" | head -1 ) > link.sh
+echo "wget --no-check-certificate " $(lynx --dump --listonly https://verge-blockchain.com/ | grep -o "https://verge-blockchain*.*zip" | head -1 ) > link.sh
 sh link.sh
 sudo rm -Rf ~/.VERGE/blocks
 sudo rm -Rf ~/.VERGE/chainstate
