3798293933af2dab630371824cbd3153c062219f
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2022-11-11 00:25:48 -0500

    update libcap

--- depends/packages/libcap.mk
+++ depends/packages/libcap.mk
@@ -1,8 +1,8 @@
 package=libcap
-$(package)_version=2.24
+$(package)_version=2.55
 $(package)_download_path=https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=3a9640e0712d545109cecef83c2614c68951e5fcf0fff4dac4df1a893c52bdbb
+$(package)_sha256_hash=047f48357cb7afbcf50bf9a60fd938cd8c5b2e4522391f826c566d1c9cc5af16
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-static
