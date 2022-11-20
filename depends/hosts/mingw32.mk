437a50bb94b462b9d03a6a2cb82cd3b1e54f7d34
justinvforvendetta <justinvforvendetta@gmail.com>
2022-11-19 20:05:36 -0500

    update mingw32 mk
    gets thru protobuf 2.6.1, boost 1.74, openssl 1.1.1n, libcap 2.55-c2, libevent-2.1.8, zlib 1.2.11

--- depends/hosts/mingw32.mk
+++ depends/hosts/mingw32.mk
@@ -1,26 +1,12 @@
-ifneq ($(shell $(SHELL) $(.SHELLFLAGS) "command -v $(host)-g++-posix"),)
-mingw32_CXX := $(host)-g++-posix
-endif
+mingw32_CFLAGS=-pipe
+mingw32_CXXFLAGS=$(mingw32_CFLAGS) -isystem $(host_prefix)/include/c++/v1
 
-mingw32_CFLAGS=-pipe -std=$(C_STANDARD)
-mingw32_CXXFLAGS=-pipe -std=$(CXX_STANDARD)
+mingw32_LDFLAGS?=-fuse-ld=lld
 
-ifneq ($(LTO),)
-mingw32_CFLAGS += -flto
-mingw32_CXXFLAGS += -flto
-mingw32_LDFLAGS += -flto
-
-mingw32_AR = $(host_toolchain)gcc-ar
-mingw32_NM = $(host_toolchain)gcc-nm
-mingw32_RANLIB = $(host_toolchain)gcc-ranlib
-endif
-
-mingw32_release_CFLAGS=-O2
+mingw32_release_CFLAGS=-O3
 mingw32_release_CXXFLAGS=$(mingw32_release_CFLAGS)
 
-mingw32_debug_CFLAGS=-O1
+mingw32_debug_CFLAGS=-O0
 mingw32_debug_CXXFLAGS=$(mingw32_debug_CFLAGS)
 
 mingw32_debug_CPPFLAGS=-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
-
-mingw32_cmake_system=Windows
