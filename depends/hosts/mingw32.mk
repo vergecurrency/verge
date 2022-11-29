7aa789c79049b9794ca3101d3c3366a8b8d75bff
justinvforvendetta <justinvforvendetta@gmail.com>
2022-11-28 22:58:21 -0500

    update ming32 host macro

--- depends/hosts/mingw32.mk
+++ depends/hosts/mingw32.mk
@@ -1,12 +1,28 @@
+ifneq ($(shell $(SHELL) $(.SHELLFLAGS) "command -v $(host)-g++-posix"),)
+mingw32_CXX := $(host)-g++-posix
+endif
+
 mingw32_CFLAGS=-pipe
-mingw32_CXXFLAGS=$(mingw32_CFLAGS) -isystem $(host_prefix)/include/c++/v1
+mingw32_CXXFLAGS=-pipe
 
-mingw32_LDFLAGS?=-fuse-ld=lld
+ifneq ($(LTO),)
+mingw32_CFLAGS += -flto
+mingw32_CXXFLAGS += -flto
+mingw32_LDFLAGS += -flto
 
-mingw32_release_CFLAGS=-O3
+mingw32_AR = $(host_toolchain)gcc-ar
+mingw32_NM = $(host_toolchain)gcc-nm
+mingw32_RANLIB = $(host_toolchain)gcc-ranlib
+endif
+
+#mingw32_LDFLAGS?=-fuse-ld=lld
+
+mingw32_release_CFLAGS=-O2
 mingw32_release_CXXFLAGS=$(mingw32_release_CFLAGS)
 
-mingw32_debug_CFLAGS=-O0
+mingw32_debug_CFLAGS=-O1
 mingw32_debug_CXXFLAGS=$(mingw32_debug_CFLAGS)
 
 mingw32_debug_CPPFLAGS=-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
+
+mingw32_cmake_system=Windows
