549cd1ca5d231959c3e34678b38143e1ad7cb6fb
justinvforvendetta <justinvforvendetta@gmail.com>
2022-11-28 23:01:47 -0500

    use double quotes

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,7 @@ export LC_ALL=C
 set -e
 git submodule update --init --recursive
 
-srcdir="$(dirname $0)"
+srcdir="$(dirname "$0")"
 cd "$srcdir"
 if [ -z "${LIBTOOLIZE}" ] && GLIBTOOLIZE="$(command -v glibtoolize)"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
