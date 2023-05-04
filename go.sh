faa19eef8bb4deec268e0b4bd19b657adad800dc
Patrick Julien <29350086+badbrainIRC@users.noreply.github.com>
2023-05-04 02:27:53 -0400

    Update go.sh

--- go.sh
+++ go.sh
@@ -325,7 +325,7 @@ cd ~
 echo -n "Success....Blockchain is now downloading press Ctrl-C to cancel but it will take longer to sync from 0. And you will have to start verge manual"
 sudo rm QT-Wallet*.zip
 # echo "wget --no-check-certificate " $(lynx --dump --listonly https://verge-blockchain.com/ | grep -o "https://verge-blockchain*.*zip" | head -1 ) > link.sh
-echo "wget --no-check-certificate https://verge-blockchain.com/downloads/"$(lynx --source https://verge-blockchain.com | grep -o "QT-Wallet*.*zip" | head -1 ) > link.sh
+echo "wget --no-check-certificate https://verge-blockchain.com/download/"$(lynx --source https://verge-blockchain.com | grep -o "QT-Wallet*.*zip" | head -1 ) > link.sh
 sh link.sh
 sudo rm -Rf ~/.VERGE/blocks
 sudo rm -Rf ~/.VERGE/chainstate
