86f0fa98d3886016dcc018e2e6e89b03c3cfbeff
justinvforvendetta <justinvforvendetta@gmail.com>
2023-06-21 19:57:22 -0400

    updates for building qt

--- depends/packages/libxcb.mk
+++ depends/packages/libxcb.mk
@@ -3,7 +3,7 @@ $(package)_version=1.10
 $(package)_download_path=https://xcb.freedesktop.org/dist
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=98d9ab05b636dd088603b64229dd1ab2d2cc02ab807892e107d674f9c3f2d5b5
-$(package)_dependencies=xcb_proto libXau xproto
+$(package)_dependencies=xcb_proto libXau
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-static --disable-build-docs --without-doxygen --without-launchd
@@ -15,7 +15,7 @@ $(package)_config_opts += --disable-composite --disable-damage --disable-dpms
 $(package)_config_opts += --disable-dri2 --disable-dri3 --disable-glx
 $(package)_config_opts += --disable-present --disable-randr --disable-record
 $(package)_config_opts += --disable-render --disable-resource --disable-screensaver
-$(package)_config_opts += --disable-shape --disable-shm --disable-sync
+$(package)_config_opts += --disable-shape --disable-sync
 $(package)_config_opts += --disable-xevie --disable-xfixes --disable-xfree86-dri
 $(package)_config_opts += --disable-xinerama --disable-xinput
 $(package)_config_opts += --disable-xprint --disable-selinux --disable-xtest
