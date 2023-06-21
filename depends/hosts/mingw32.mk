6890703e2e0163e7d2441479b190e113cf30edff
justinvforvendetta <justinvforvendetta@gmail.com>
2023-06-21 19:46:01 -0400

    simplify win cross compile

--- depends/hosts/mingw32.mk
+++ depends/hosts/mingw32.mk
@@ -1,21 +1,5 @@
-ifneq ($(shell $(SHELL) $(.SHELLFLAGS) "command -v $(host)-g++-posix"),)
-mingw32_CXX := $(host)-g++-posix
-endif
-
 mingw32_CFLAGS=-pipe
-mingw32_CXXFLAGS=-pipe
-
-ifneq ($(LTO),)
-mingw32_CFLAGS += -flto
-mingw32_CXXFLAGS += -flto
-mingw32_LDFLAGS += -flto
-
-mingw32_AR = $(host_toolchain)gcc-ar
-mingw32_NM = $(host_toolchain)gcc-nm
-mingw32_RANLIB = $(host_toolchain)gcc-ranlib
-endif
-
-#mingw32_LDFLAGS?=-fuse-ld=lld
+mingw32_CXXFLAGS=$(mingw32_CFLAGS)
 
 mingw32_release_CFLAGS=-O2
 mingw32_release_CXXFLAGS=$(mingw32_release_CFLAGS)
@@ -25,4 +9,4 @@ mingw32_debug_CXXFLAGS=$(mingw32_debug_CFLAGS)
 
 mingw32_debug_CPPFLAGS=-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
 
-mingw32_cmake_system=Windows
+mingw_cmake_system=Windows
