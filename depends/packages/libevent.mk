47e3ef85c81e90c8a4c47658bd6038da7dae37f4
justinvforvendetta <justinvforvendetta@gmail.com>
2023-06-21 18:05:47 -0400

    update libevent & qr code lib
    see title or commit itself for more info.

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -1,5 +1,5 @@
 package=libevent
-$(package)_version=2.1.8-stable
+$(package)_version=2.1.12-stable
 $(package)_download_path=https://github.com/libevent/libevent/archive/
 $(package)_file_name=release-$($(package)_version).tar.gz
 $(package)_sha256_hash=316ddb401745ac5d222d7c529ef1eada12f58f6376a66c1118eee803cb70f83d
