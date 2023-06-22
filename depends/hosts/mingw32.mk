88031614af1621c83ac7ca99cb723c1dfc4a15d0
justinvforvendetta <justinvforvendetta@gmail.com>
2023-06-22 18:35:56 -0400

    update compile flag checker to serial 6

--- depends/hosts/mingw32.mk
+++ depends/hosts/mingw32.mk
@@ -2,18 +2,9 @@ ifneq ($(shell $(SHELL) $(.SHELLFLAGS) "command -v $(host)-g++-posix"),)
 mingw32_CXX := $(host)-g++-posix
 endif
 
-mingw32_CFLAGS=-pipe -std=$(C_STANDARD)
-mingw32_CXXFLAGS=-pipe -std=$(CXX_STANDARD)
-
-ifneq ($(LTO),)
-mingw32_CFLAGS += -flto
-mingw32_CXXFLAGS += -flto
-mingw32_LDFLAGS += -flto
-
-mingw32_AR = $(host_toolchain)gcc-ar
-mingw32_NM = $(host_toolchain)gcc-nm
-mingw32_RANLIB = $(host_toolchain)gcc-ranlib
-endif
+mingw32_CFLAGS=-pipe
+mingw32_CXXFLAGS=$(mingw32_CFLAGS)
+mingw32_ARFLAGS=cr
 
 mingw32_release_CFLAGS=-O2
 mingw32_release_CXXFLAGS=$(mingw32_release_CFLAGS)
