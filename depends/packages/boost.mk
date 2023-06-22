575a5a37fdc76f645d89a417fe4f167cf1e79c6a
justinvforvendetta <justinvforvendetta@gmail.com>
2023-06-21 21:42:02 -0400

    remove jobcount from making b2

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -35,9 +35,9 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  b2 -d2 -j$(JOBCOUNT) -d1 --prefix=$($(package)_staging_prefix_dir) $($(package)_config_opts) toolset=$($(package)_toolset_$(host_os)) stage
+  b2 -d2 -d1 --prefix=$($(package)_staging_prefix_dir) $($(package)_config_opts) toolset=$($(package)_toolset_$(host_os)) stage
 endef
 
 define $(package)_stage_cmds
-  b2 -d0 -j$(JOBCOUNT) --prefix=$($(package)_staging_prefix_dir) $($(package)_config_opts) toolset=$($(package)_toolset_$(host_os)) install
+  b2 -d0 --prefix=$($(package)_staging_prefix_dir) $($(package)_config_opts) toolset=$($(package)_toolset_$(host_os)) install
 endef
