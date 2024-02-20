49f8394502eb11f328e6cdb9638f51400d4b37be
sunerok <justinvforvendetta@gmail.com>
2024-02-20 16:09:29 -0500

    update more Xorg libs

--- depends/packages/xcb_proto.mk
+++ depends/packages/xcb_proto.mk
@@ -1,12 +1,16 @@
 package=xcb_proto
-$(package)_version=1.10
+$(package)_version=1.13
 $(package)_download_path=http://xcb.freedesktop.org/dist
 $(package)_file_name=xcb-proto-$($(package)_version).tar.bz2
-$(package)_sha256_hash=7ef40ddd855b750bc597d2a435da21e55e502a0fefa85b274f2c922800baaf05
+$(package)_sha256_hash=7b98721e669be80284e9bbfeab02d2d0d54cd11172b72271e47a2fe875e2bde1
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared
   $(package)_config_opts_linux=--with-pic
+endef
+
+define $(package)_preprocess_cmds
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
 endef
 
 define $(package)_config_cmds
--- depends/packages/xtrans.mk
+++ depends/packages/xtrans.mk
@@ -1,8 +1,8 @@
 package=xtrans
-$(package)_version=1.3.4
+$(package)_version=1.4.0
 $(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=054d4ee3efd52508c753e9f7bc655ef185a29bd2850dd9e2fc2ccc33544f583a
+$(package)_sha256_hash=377c4491593c417946efcd2c7600d1e62639f7a8bbca391887e2c4679807d773
 $(package)_dependencies=
 
 define $(package)_set_vars
