22c99ac79d1b60591b5294e07b2434e9c2a91390
sunerok <justinvforvendetta@gmail.com>
2024-02-20 17:03:03 -0500

    update libxcb

--- depends/packages/libxcb.mk
+++ depends/packages/libxcb.mk
@@ -1,15 +1,16 @@
 package=libxcb
-$(package)_version=1.14
+$(package)_version=1.16
 $(package)_download_path=https://xcb.freedesktop.org/dist
-$(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_sha256_hash=a55ed6db98d43469801262d81dc2572ed124edc3db31059d4e9916eb9f844c34
+$(package)_file_name=libxcb-$($(package)_version).tar.xz
+$(package)_sha256_hash=4348566aa0fbf196db5e0a576321c65966189210cb51328ea2bb2be39c711d71
 $(package)_dependencies=xcb_proto libXau
+$(package)_patches = remove_pthread_stubs.patch
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-static --disable-devel-docs --without-doxygen --without-launchd
+$(package)_config_opts=--disable-shared --disable-devel-docs --without-doxygen --without-launchd
 $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
-# Disable uneeded extensions.
-# More info is available from: https://doc.qt.io/qt-5.15/linux-requirements.html
+# Disable unneeded extensions.
+# More info is available from: https://doc.qt.io/qt-6.5/linux-requirements.html
 $(package)_config_opts += --disable-composite --disable-damage --disable-dpms
 $(package)_config_opts += --disable-dri2 --disable-dri3 --disable-glx
 $(package)_config_opts += --disable-present --disable-record --disable-resource
@@ -19,17 +20,12 @@ $(package)_config_opts += --disable-xtest --disable-xv --disable-xvmc
 endef
 
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux &&\
-  sed "s/pthread-stubs//" -i configure
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux && \
+  patch -p1 -i $($(package)_patch_dir)/remove_pthread_stubs.patch
 endef
 
-# Don't install xcb headers to the default path in order to work around a qt
-# build issue: https://bugreports.qt.io/browse/QTBUG-34748
-# When using qt's internal libxcb, it may end up finding the real headers in
-# depends staging. Use a non-default path to avoid that.
-
 define $(package)_config_cmds
-  $($(package)_autoconf) --includedir=$(host_prefix)/include/xcb-shared
+  $($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
@@ -41,5 +37,5 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  rm -rf share/man share/doc lib/*.la
+  rm -rf share lib/*.la
 endef
