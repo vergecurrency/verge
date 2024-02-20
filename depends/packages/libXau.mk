fa952b8d702486fd08de8682c1e489aebcee31b4
sunerok <justinvforvendetta@gmail.com>
2024-02-20 17:30:02 -0500

    update Xau

--- depends/packages/libXau.mk
+++ depends/packages/libXau.mk
@@ -1,12 +1,13 @@
 package=libXau
-$(package)_version=1.0.8
+$(package)_version=1.0.9
 $(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=fdd477320aeb5cdd67272838722d6b7d544887dfe7de46e1e7cc0c27c2bea4f2
+$(package)_sha256_hash=ccf8cbf0dbf676faa2ea0a6d64bcc3b6746064722b606c8c52917ed00dcb73ec
 $(package)_dependencies=xproto
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared
+  $(package)_config_opts=--disable-shared --disable-lint-library --without-lint
+  $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
   $(package)_config_opts_linux=--with-pic
 endef
 
@@ -24,4 +25,8 @@ endef
 
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+  rm lib/*.la
 endef
