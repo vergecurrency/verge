14f0be204c175c344006161653029b540a2ade47
sunerok <justinvforvendetta@gmail.com>
2024-02-20 16:34:05 -0500

    update more Xorg libs

--- depends/packages/libxcb.mk
+++ depends/packages/libxcb.mk
@@ -1,9 +1,9 @@
 package=libxcb
-$(package)_version=1.10
-$(package)_download_path=https://xcb.freedesktop.org/dist
+$(package)_version=1.13
+$(package)_download_path=http://xcb.freedesktop.org/dist
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=98d9ab05b636dd088603b64229dd1ab2d2cc02ab807892e107d674f9c3f2d5b5
-$(package)_dependencies=xcb_proto libXau
+$(package)_sha256_hash=188c8752193c50ff2dbe89db4554c63df2e26a2e47b0fa415a70918b5b851daa
+$(package)_dependencies=xcb_proto libXau xproto
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-static --disable-build-docs --without-doxygen --without-launchd
@@ -15,20 +15,25 @@ $(package)_config_opts += --disable-composite --disable-damage --disable-dpms
 $(package)_config_opts += --disable-dri2 --disable-dri3 --disable-glx
 $(package)_config_opts += --disable-present --disable-randr --disable-record
 $(package)_config_opts += --disable-render --disable-resource --disable-screensaver
-$(package)_config_opts += --disable-shape --disable-sync
+$(package)_config_opts += --disable-shape --disable-shm --disable-sync
 $(package)_config_opts += --disable-xevie --disable-xfixes --disable-xfree86-dri
-$(package)_config_opts += --disable-xinerama --disable-xinput
+$(package)_config_opts += --disable-xinerama --disable-xinput --disable-xkb
 $(package)_config_opts += --disable-xprint --disable-selinux --disable-xtest
 $(package)_config_opts += --disable-xv --disable-xvmc
 endef
 
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux && \
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux &&\
   sed "s/pthread-stubs//" -i configure
 endef
 
+# Don't install xcb headers to the default path in order to work around a qt
+# build issue: https://bugreports.qt.io/browse/QTBUG-34748
+# When using qt's internal libxcb, it may end up finding the real headers in
+# depends staging. Use a non-default path to avoid that.
+
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  $($(package)_autoconf) --includedir=$(host_prefix)/include/xcb-shared
 endef
 
 define $(package)_build_cmds
@@ -40,5 +45,5 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  rm -rf share lib/*.la
+  rm -rf share/man share/doc lib/*.la
 endef
