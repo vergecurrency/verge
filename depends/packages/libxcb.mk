914bd346a67779d1fc5132df3a3f49081a575032
sunerok <justinvforvendetta@gmail.com>
2024-02-20 17:29:06 -0500

    fix xcb for X11 compile

--- depends/packages/libxcb.mk
+++ depends/packages/libxcb.mk
@@ -1,8 +1,28 @@
-package=xcb_proto
-$(package)_version=1.16.0
-$(package)_download_path=https://xorg.freedesktop.org/archive/individual/proto
-$(package)_file_name=xcb-proto-$($(package)_version).tar.xz
-$(package)_sha256_hash=a75a1848ad2a89a82d841a51be56ce988ff3c63a8d6bf4383ae3219d8d915119
+package=libxcb
+$(package)_version=1.16
+$(package)_download_path=https://xcb.freedesktop.org/dist
+$(package)_file_name=libxcb-$($(package)_version).tar.xz
+$(package)_sha256_hash=4348566aa0fbf196db5e0a576321c65966189210cb51328ea2bb2be39c711d71
+$(package)_dependencies=xcb_proto libXau
+$(package)_patches = remove_pthread_stubs.patch
+
+define $(package)_set_vars
+$(package)_config_opts=--disable-shared --disable-devel-docs --without-doxygen --without-launchd
+$(package)_config_opts += --disable-dependency-tracking --enable-option-checking
+# Disable unneeded extensions.
+# More info is available from: https://doc.qt.io/qt-6.5/linux-requirements.html
+$(package)_config_opts += --disable-composite --disable-damage --disable-dpms
+$(package)_config_opts += --disable-dri2 --disable-dri3 --disable-glx
+$(package)_config_opts += --disable-present --disable-record --disable-resource
+$(package)_config_opts += --disable-screensaver --disable-xevie --disable-xfree86-dri
+$(package)_config_opts += --disable-xinput --disable-xprint --disable-selinux
+$(package)_config_opts += --disable-xtest --disable-xv --disable-xvmc
+endef
+
+define $(package)_preprocess_cmds
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux && \
+  patch -p1 -i $($(package)_patch_dir)/remove_pthread_stubs.patch
+endef
 
 define $(package)_config_cmds
   $($(package)_autoconf)
@@ -17,5 +37,5 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  rm -rf lib/python*/site-packages/xcbgen/__pycache__
+  rm -rf share lib/*.la
 endef
