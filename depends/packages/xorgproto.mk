7e06133248d7dc666543cf6ad507739e986b8d5c
sunerok <justinvforvendetta@gmail.com>
2024-05-07 16:27:49 -0400

    fix proto

--- depends/packages/xorgproto.mk
+++ depends/packages/xorgproto.mk
@@ -1,18 +1,15 @@
 package=xorgproto
-$(package)_version=2024.1
-$(package)_download_path=https://xorg.freedesktop.org/archive/individual/proto
-$(package)_file_name=xorgproto-$($(package)_version).tar.xz
-$(package)_sha256_hash=372225fd40815b8423547f5d890c5debc72e88b91088fbfb13158c20495ccb59
+$(package)_version=7.0.31
+$(package)_download_path=http://xorg.freedesktop.org/releases/individual/proto
+$(package)_file_name=$(package)-$($(package)_version).tar.bz2
+$(package)_sha256_hash=c6f9747da0bd3a95f86b17fb8dd5e717c8f3ab7f0ece3ba1b247899ec1ef7747
 
 define $(package)_set_vars
-  $(package)_config_opts=--without-fop --without-xmlto --without-xsltproc --disable-specs
-  $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
+$(package)_config_opts=--without-fop --without-xmlto --without-xsltproc --disable-specs
+$(package)_config_opts += --disable-dependency-tracking --enable-option-checking
 endef
 
 define $(package)_preprocess_cmds
-  find . -type f -name 'Makefile.in' -exec rm {} + && \
-  rm aclocal.m4 compile config.guess config.sub configure install-sh missing test-driver && \
-  autoreconf -fi && \
   cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
 endef
 
