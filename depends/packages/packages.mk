a00d6345ee1fc3d691b10fa9ed5901cbeafe0db8
sunerok <justinvforvendetta@gmail.com>
2024-05-07 16:07:34 -0400

    change name update in packages

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -3,7 +3,7 @@ packages:=boost openssl libevent libcap libseccomp zeromq
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xorgproto freetype fontconfig libX11 xextproto libXext xtrans
 
 rapidcheck_packages = rapidcheck
 
