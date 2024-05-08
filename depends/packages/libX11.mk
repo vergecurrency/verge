86112ec88629d9c8b70a0cb576ea1ac687fc3255
sunerok <justinvforvendetta@gmail.com>
2024-05-07 20:18:54 -0400

    update xproto to xorgproto

--- depends/packages/libX11.mk
+++ depends/packages/libX11.mk
@@ -3,7 +3,7 @@ $(package)_version=1.6.9
 $(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=9cc7e8d000d6193fa5af580d50d689380b8287052270f5bb26a5fb6b58b2bed1
-$(package)_dependencies=libxcb xtrans xextproto xproto
+$(package)_dependencies=libxcb xtrans xextproto xorgproto
 $(package)_patches=patch-malloc-zero-check.patch
 
 define $(package)_set_vars
--- depends/packages/libXext.mk
+++ depends/packages/libXext.mk
@@ -3,7 +3,7 @@ $(package)_version=1.3.4
 $(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=59ad6fcce98deaecc14d39a672cf218ca37aba617c9a0f691cac3bcd28edf82b
-$(package)_dependencies=xproto xextproto libX11 libXau
+$(package)_dependencies=xorgproto xextproto libX11 libXau
 $(package)_patches=patch-malloc-zero-check.patch
 
 define $(package)_set_vars
