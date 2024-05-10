6cc75a44328e4dd2bf13e84357e71de595bce937
sunerok <justinvforvendetta@gmail.com>
2024-05-10 18:21:16 -0400

    patch dbus

--- depends/packages/dbus.mk
+++ depends/packages/dbus.mk
@@ -4,10 +4,15 @@ $(package)_download_path=https://dbus.freedesktop.org/releases/dbus
 $(package)_file_name=$(package)-$($(package)_version).tar.xz
 $(package)_sha256_hash=84FC597E6EC82F05DC18A7D12C17046F95BAD7BE99FC03C15BC254C4701ED204
 $(package)_dependencies=expat
+$(package)_patches=remove-DDBUS_STATIC_BUILD.patch
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared --disable-maintainer-mode --disable-xml-docs --disable-doxygen-docs
   $(package)_config_opts+=--disable-ducktype-docs
+endef
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/remove-DDBUS_STATIC_BUILD.patch
 endef
 
 define $(package)_config_cmds
