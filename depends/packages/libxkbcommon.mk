b05cc8afde42b3a257b215eeb8d7b2fbf5997d22
sunerok <justinvforvendetta@gmail.com>
2024-06-04 17:15:09 -0400

    libxcb/xkbcommon

--- depends/packages/libxkbcommon.mk
+++ depends/packages/libxkbcommon.mk
@@ -1,31 +1,31 @@
 package=libxkbcommon
-$(package)_version=0.8.4
+$(package)_version=1.7.0
 $(package)_download_path=https://xkbcommon.org/download/
-$(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_sha256_hash=60ddcff932b7fd352752d51a5c4f04f3d0403230a584df9a2e0d5ed87c486c8b
+$(package)_file_name=libxkbcommon-$($(package)_version).tar.xz
+$(package)_sha256_hash=65782f0a10a4b455af9c6baab7040e2f537520caa2ec2092805cdfd36863b247
 $(package)_dependencies=libxcb
-
-define $(package)_set_vars
-$(package)_config_opts = --enable-option-checking --disable-dependency-tracking
-$(package)_config_opts += --disable-static --disable-docs
-endef
+$(package)_patches=no-test-x11.patch toolchain.txt
 
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux
+  patch -p1 < $($(package)_patch_dir)/no-test-x11.patch && \
+  cp $($(package)_patch_dir)/toolchain.txt toolchain.txt && \
+  sed -i -e 's|@host_prefix@|$(host_prefix)|' \
+         -e 's|@cc@|$($(package)_cc)|' \
+         -e 's|@cxx@|$($(package)_cxx)|' \
+         -e 's|@ar@|$($(package)_ar)|' \
+         -e 's|@strip@|$(host_STRIP)|' \
+         -e 's|@arch@|$(host_arch)|' \
+	  toolchain.txt
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  meson setup --cross-file toolchain.txt build
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  ninja -C build
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
-endef
-
-define $(package)_postprocess_cmds
-  rm lib/*.la
+  DESTDIR=$($(package)_staging_dir) ninja -C build install
 endef
