e8c9f5835a8dfedc87868ee6a87fe01b6bfcf984
sunerok <justinvforvendetta@gmail.com>
2024-08-22 19:09:58 -0400

    new libevent needs openssl to create bufferevent_ssl.h!
    found it! big change in libevent-2.1.12: [autotools: do not install bufferevent_ssl.h under --disable-openssl (5349a07e Azat Khuzhin)]

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -3,13 +3,14 @@ $(package)_version=2.1.12-stable
 $(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb
+$(package)_dependencies=openssl
 $(package)_patches+=fix_mingw_link.patch
-
+x
 # When building for Windows, we set _WIN32_WINNT to target the same Windows
 # version as we do in configure. Due to quirks in libevents build system, this
 # is also required to enable support for ipv6. See #19375.
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress --disable-samples
+  $(package)_config_opts=--disable-shared --with-openssl --disable-libevent-regress --disable-samples
   $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
   $(package)_config_opts_release=--disable-debug-mode
   $(package)_config_opts_linux=--with-pic
