13ec4e69fae64c3419a2b2590cfd4dc9ce245e78
sunerok <justinvforvendetta@gmail.com>
2024-08-22 20:05:59 -0400

    ok, 2.1.11 it is.
    tor isnt ready for 2.1.12, but at least we upgraded from 2.1.8

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -1,16 +1,16 @@
 package=libevent
-$(package)_version=2.1.12-stable
-$(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)/
-$(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb
+$(package)_version=2.1.11-stable
+$(package)_download_path=https://github.com/libevent/libevent/archive/
+$(package)_file_name=release-$($(package)_version).tar.gz
+$(package)_sha256_hash=229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e
 $(package)_dependencies=openssl
-$(package)_patches+=fix_mingw_link.patch
+#$(package)_patches+=fix_mingw_link.patch
 
 # When building for Windows, we set _WIN32_WINNT to target the same Windows
 # version as we do in configure. Due to quirks in libevents build system, this
 # is also required to enable support for ipv6. See #19375.
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared --with-openssl --disable-libevent-regress --disable-samples
+  $(package)_config_opts=--disable-shared --disable-libevent-regress --disable-samples
   $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
   $(package)_config_opts_release=--disable-debug-mode
   $(package)_config_opts_linux=--with-pic
@@ -23,8 +23,13 @@ define $(package)_set_vars
 endef
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/fix_mingw_link.patch && \
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux
+  ./autogen.sh
+endef
+
+define $(package)_set_vars
+  $(package)_config_opts=--disable-shared --disable-libevent-regress --disable-samples
+  $(package)_config_opts_release=--disable-debug-mode
+  $(package)_config_opts_linux=--with-pic
 endef
 
 define $(package)_config_cmds
