b8d1a8f8cc747d4818aada6f16abcbb45cd130a2
sunerok <justinvforvendetta@gmail.com>
2024-09-03 17:25:11 -0400

    Update dbwrapper.cpp

--- src/dbwrapper.cpp
+++ src/dbwrapper.cpp
@@ -116,7 +116,7 @@ static leveldb::Options GetOptions(size_t nCacheSize)
     return options;
 }
 
-CDBWrapper::CDBWrapper(const boost::filesystem::path& path, size_t nCacheSize, bool fMemory, bool fWipe, bool obfuscate)
+CDBWrapper::CDBWrapper(const fs::path& path, size_t nCacheSize, bool fMemory, bool fWipe, bool obfuscate)
 	: m_name(fs::basename(path))
 {
     penv = NULL;
