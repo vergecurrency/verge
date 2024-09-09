5107f1b4835cb450aa83b6be469db00613479681
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2024-09-09 11:06:33 -0400

    bump Cap’n Proto 1.0.1 to 1.0.2
    cerealization protocol.

--- depends/packages/native_capnp.mk
+++ depends/packages/native_capnp.mk
@@ -1,9 +1,9 @@
 package=native_capnp
-$(package)_version=1.0.1
+$(package)_version=1.0.2
 $(package)_download_path=https://capnproto.org/
 $(package)_download_file=capnproto-c++-$($(package)_version).tar.gz
 $(package)_file_name=capnproto-cxx-$($(package)_version).tar.gz
-$(package)_sha256_hash=0f7f4b8a76a2cdb284fddef20de8306450df6dd031a47a15ac95bc43c3358e09
+$(package)_sha256_hash=9057DBC0223366B74BBECA33A05DE164A229B0377927F1B7EF3828CDD8CB1D7E
 
 define $(package)_set_vars
   $(package)_config_opts := -DBUILD_TESTING=OFF
