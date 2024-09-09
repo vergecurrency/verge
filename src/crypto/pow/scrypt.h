dd554e45e861103da6b3b3c099165df92af58ba5
sunerok <justinvforvendetta@gmail.com>
2024-09-09 19:00:29 -0400

    change have to use, in scrypt header file

--- src/crypto/pow/scrypt.h
+++ src/crypto/pow/scrypt.h
@@ -12,7 +12,7 @@ static const int SCRYPT_SCRATCHPAD_SIZE = 131072 + 63;
 void scrypt_1024_1_1_256(const char *input, char *output);
 void scrypt_1024_1_1_256_sp_generic(const char *input, char *output, char *scratchpad);
 
-#if defined(HAVE_SSE2)
+#if defined(USE_SSE2)
 #if defined(_M_X64) || defined(__x86_64__) || defined(_M_AMD64) || (defined(MAC_OSX) && defined(__i386__))
 #define USE_SSE2_ALWAYS 1
 #define scrypt_1024_1_1_256_sp(input, output, scratchpad) scrypt_1024_1_1_256_sp_sse2((input), (output), (scratchpad))
