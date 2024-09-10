e8aa376e9e7477fec127129b695b715828ccb05b
sunerok <justinvforvendetta@gmail.com>
2024-09-10 08:35:56 -0400

    Update scrypt.cpp

--- src/crypto/pow/scrypt.cpp
+++ src/crypto/pow/scrypt.cpp
@@ -61,7 +61,6 @@ static inline uint32_t be32dec(const void *pp)
 	return ((uint32_t)(p[3]) + ((uint32_t)(p[2]) << 8) +
 	    ((uint32_t)(p[1]) << 16) + ((uint32_t)(p[0]) << 24));
 }
-*/
 
 static inline void be32enc(void *pp, uint32_t x)
 {
