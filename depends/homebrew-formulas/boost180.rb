6677531235ff0a2642d069d9ba44fc0a1f6cfdcc
sunerok <justinvforvendetta@gmail.com>
2025-01-06 08:45:29 -0500

    update brew formulae boost to 1.80
    and pray.

--- depends/homebrew-formulas/boost180.rb
+++ depends/homebrew-formulas/boost180.rb
@@ -1,23 +1,18 @@
-class Boost176 < Formula
+class Boost180 < Formula
   desc "Collection of portable C++ source libraries"
   homepage "https://www.boost.org/"
-  url "https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.bz2"
-  sha256 "79e6d3f986444e5a80afbeccdaf2d1c1cf964baa8d766d20859d653a16c39848"
+  url "https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.bz2"
+  sha256 "1e19565d82e43bc59209a168f5ac899d3ba471d55c7610c677d4ccf2c9c500c0"
   license "BSL-1.0"
-  revision 6
+  
   keg_only :versioned_formula
-  depends_on "icu4c@75"
+  depends_on "icu4c"
+  depends_on "xz"
+  depends_on "zstd"
+
   uses_from_macos "bzip2"
   uses_from_macos "zlib"
-  # Backport fixes for newer Clang
-  patch :p2 do
-    url "https://github.com/boostorg/numeric_conversion/commit/50a1eae942effb0a9b90724323ef8f2a67e7984a.patch?full_index=1"
-    sha256 "d96761257f7efc2edc8414f1a2522fc07a3d7d56bb55a51d14af9abd39e389c8"
-  end
-  patch :p2 do
-    url "https://github.com/boostorg/mpl/commit/b37b709cbdb6b2c285fb808dab985aa005786351.patch?full_index=1"
-    sha256 "b8013ad3e6b63698158319f5efc2fe1558a00c1d2e32193086f741e774acc3e4"
-  end
+
   def install
     # Force boost to compile with the desired compiler
     open("user-config.jam", "a") do |file|
@@ -27,19 +22,24 @@ class Boost176 < Formula
         file.write "using gcc : : #{ENV.cxx} ;\n"
       end
     end
+
     # libdir should be set by --prefix but isn't
-    icu4c_prefix = Formula["icu4c@75"].opt_prefix
+    icu4c_prefix = Formula["icu4c"].opt_prefix
     bootstrap_args = %W[
       --prefix=#{prefix}
       --libdir=#{lib}
       --with-icu=#{icu4c_prefix}
     ]
+
     # Handle libraries that will not be built.
     without_libraries = ["python", "mpi"]
+
     # Boost.Log cannot be built using Apple GCC at the moment. Disabled
     # on such systems.
     without_libraries << "log" if ENV.compiler == :gcc
+
     bootstrap_args << "--without-libraries=#{without_libraries.join(",")}"
+
     # layout should be synchronized with boost-python and boost-mpi
     args = %W[
       --prefix=#{prefix}
@@ -48,28 +48,39 @@ class Boost176 < Formula
       -j#{ENV.make_jobs}
       --layout=tagged-1.66
       --user-config=user-config.jam
-      -sNO_LZMA=1
-      -sNO_ZSTD=1
       install
       threading=multi,single
       link=shared,static
     ]
+
     # Boost is using "clang++ -x c" to select C compiler which breaks C++14
     # handling using ENV.cxx14. Using "cxxflags" and "linkflags" still works.
     args << "cxxflags=-std=c++14"
     args << "cxxflags=-stdlib=libc++" << "linkflags=-stdlib=libc++" if ENV.compiler == :clang
+
     system "./bootstrap.sh", *bootstrap_args
     system "./b2", "headers"
     system "./b2", *args
   end
+
   test do
-    (testpath/"test.cpp").write <<~CPP
+    (testpath/"test.cpp").write <<~EOS
       #include <boost/algorithm/string.hpp>
+      #include <boost/iostreams/device/array.hpp>
+      #include <boost/iostreams/device/back_inserter.hpp>
+      #include <boost/iostreams/filter/zstd.hpp>
+      #include <boost/iostreams/filtering_stream.hpp>
+      #include <boost/iostreams/stream.hpp>
+
       #include <string>
+      #include <iostream>
       #include <vector>
       #include <assert.h>
+
       using namespace boost::algorithm;
+      using namespace boost::iostreams;
       using namespace std;
+
       int main()
       {
         string str("a,b");
@@ -78,10 +89,30 @@ class Boost176 < Formula
         assert(strVec.size()==2);
         assert(strVec[0]=="a");
         assert(strVec[1]=="b");
+
+        // Test boost::iostreams::zstd_compressor() linking
+        std::vector<char> v;
+        back_insert_device<std::vector<char>> snk{v};
+        filtering_ostream os;
+        os.push(zstd_compressor());
+        os.push(snk);
+        os << "Boost" << std::flush;
+        os.pop();
+
+        array_source src{v.data(), v.size()};
+        filtering_istream is;
+        is.push(zstd_decompressor());
+        is.push(src);
+        std::string s;
+        is >> s;
+
+        assert(s == "Boost");
+
         return 0;
       }
-    CPP
-    system ENV.cxx, "-I#{Formula["boost@1.76"].opt_include}", "test.cpp", "-std=c++14", "-o", "test"
+    EOS
+    system ENV.cxx, "test.cpp", "-std=c++14", "-o", "test", "-L#{lib}", "-lboost_iostreams",
+                    "-L#{Formula["zstd"].opt_lib}", "-lzstd"
     system "./test"
   end
 end
