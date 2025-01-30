92289753f4f295e891e1b157bd6bfb849cb6c393
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2025-01-30 11:43:38 -0500

    Update fontconfig.mk

--- depends/packages/fontconfig.mk
+++ depends/packages/fontconfig.mk
@@ -1,8 +1,8 @@
 package=fontconfig
-$(package)_version=2.15.0
+$(package)_version=2.16.0
 $(package)_download_path=http://www.freedesktop.org/software/fontconfig/release/
 $(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_sha256_hash=63A0658D0E06E0FA886106452B58EF04F21F58202EA02A94C39DE0D3335D7C0E
+$(package)_sha256_hash=6A33DC555CC9BA8B10CAF7695878EF134EEB36D0AF366041F639B1DA9B6ED220
 $(package)_dependencies=freetype expat
 
 define $(package)_set_vars
