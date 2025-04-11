42a9507b57ba56aad32c9af51d726def99f29d6f
sunerok <justinvforvendetta@gmail.com>
2025-04-11 18:16:21 -0400

    update libcap from 2.73 to 2.75

--- depends/packages/libcap.mk
+++ depends/packages/libcap.mk
@@ -1,8 +1,8 @@
 package=libcap
-$(package)_version=2.73
+$(package)_version=2.75
 $(package)_download_path=https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=16D8E4E625D2A2ED1601172704C560DE3745BAC64AB1384A8074379C03165948
+$(package)_sha256_hash=F58898263CF4A20DA5920E4340B3FF7ADDFA5021B48D79E7AD6B97C196557998
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-static
