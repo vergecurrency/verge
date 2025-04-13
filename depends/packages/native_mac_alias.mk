e181f8b25b9410f6fdd4ca7c965deddc4537d4ae
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2025-04-13 15:34:14 -0400

    native_mac_alias 2.0.7 to 2.2.2

--- depends/packages/native_mac_alias.mk
+++ depends/packages/native_mac_alias.mk
@@ -1,8 +1,8 @@
 package=native_mac_alias
-$(package)_version=2.0.7
+$(package)_version=2.2.2
 $(package)_download_path=https://github.com/al45tair/mac_alias/archive/
 $(package)_file_name=v$($(package)_version).tar.gz
-$(package)_sha256_hash=6f606d3b6bccd2112aeabf1a063f5b5ece87005a5d7e97c8faca23b916e88838
+$(package)_sha256_hash=57B1BFFF3F942F089E1BBCDA2A8CC5F300744D84DAD9E624DF832BF55CDB6218
 $(package)_install_libdir=$(build_prefix)/lib/python/dist-packages
 
 define $(package)_build_cmds
