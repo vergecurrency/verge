7ff5e5915242c38de2421dcb40d14ff54edb3f3a
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2025-04-14 14:29:34 -0400

    native_protobuf.mk 3.10.1 to 3.20.2

--- depends/packages/native_protobuf.mk
+++ depends/packages/native_protobuf.mk
@@ -1,8 +1,8 @@
 package=native_protobuf
-$(package)_version=3.10.1
+$(package)_version=3.20.2
 $(package)_download_path=https://github.com/google/protobuf/releases/download/v$($(package)_version)
 $(package)_file_name=protobuf-cpp-$($(package)_version).tar.gz
-$(package)_sha256_hash=E82EE5BDDE198E0A1935E280748A86A7989474EA771418A2FD90F03E2E65B99B
+$(package)_sha256_hash=a0167e2ba24bba0a180fbc9392f3a43e749d7a26e630fe9c1a1ba32a53675ac3
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared
