4e0037fce23254df610d7de228a9a5af51975ca3
sunerok <justinvforvendetta@gmail.com>
2025-04-28 13:59:40 -0400

    protobuf 3.16.1
    pinpoint where proto is having a tantrum

--- depends/packages/native_protobuf.mk
+++ depends/packages/native_protobuf.mk
@@ -1,8 +1,8 @@
 package=native_protobuf
-$(package)_version=3.15.1
+$(package)_version=3.16.1
 $(package)_download_path=https://github.com/google/protobuf/releases/download/v$($(package)_version)
 $(package)_file_name=protobuf-cpp-$($(package)_version).tar.gz
-$(package)_sha256_hash=D12060F1A5AF8C12DE7B5631465E3F6F29BE0AE218396C1E75EE89CACFD4B739
+$(package)_sha256_hash=CC42F7890761BD2FBBD6E9BB7D68082D5ADFFA359C626784C408549E4184FEB3
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared
