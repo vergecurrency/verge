c9bbb7d86ad8675ca0499d9afb621aab4b46462c
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2025-05-21 19:45:04 -0400

    add a seed to chainparams

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -203,6 +203,7 @@ vSeeds.emplace_back("a334wu6sx5tc5kg2gxwpvqb5pcs2vgitvtiqbaksyhvviwsgasauteid.on
 vSeeds.emplace_back("irilna4hmti5q6n7cniyqvnp3v6iq4lnjnj2vowelmkueuw7dpehq6id.onion");
 vSeeds.emplace_back("p4vjqpnqslrc2gniopxgu75t2hwdjrhr56unzxyjmlvxh7l6dk33tlqd.onion");
 vSeeds.emplace_back("yacufiue4c45bg2joifyuzpvl35fm3jrf7fvy2q5n7mvu22dbxum7vid.onion");
+vSeeds.emplace_back("aj5ykqxwgesuruyalrlm75jvu3im6yrbm5omlzesooj4g6ojpaleziad.onion");
 }
 
 if (gArgs.IsArgSet("-without-tor") || gArgs.GetBoolArg("-dynamic-network", false)) {
