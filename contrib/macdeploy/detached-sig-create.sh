352c2e25dd92ddf6932a067ef33d9ebea7e01760
sunerok <justinvforvendetta@gmail.com>
2025-06-20 21:26:18 -0400

    change boost dep source and keep mac dmg naming consistent
    jfrog boost server is offline? use boost org instead

--- contrib/macdeploy/detached-sig-create.sh
+++ contrib/macdeploy/detached-sig-create.sh
@@ -7,7 +7,7 @@ export LC_ALL=C
 set -e
 
 ROOTDIR=dist
-BUNDLE="${ROOTDIR}/verge-qt.app"
+BUNDLE="${ROOTDIR}/Verge-Qt.app"
 CODESIGN=codesign
 TEMPDIR=sign.temp
 TEMPLIST=${TEMPDIR}/signatures.txt
--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,6 +1,6 @@
 package=boost
 $(package)_version=1.80.0
-$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$($(package)_version)/source/
+$(package)_download_path=https://archives.boost.io/release/$($(package)_version)/source/
 $(package)_file_name=boost_$(subst .,_,$($(package)_version)).tar.bz2
 $(package)_sha256_hash=1e19565d82e43bc59209a168f5ac899d3ba471d55c7610c677d4ccf2c9c500c0
 
