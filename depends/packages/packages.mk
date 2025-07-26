1f655c13650478ad4771aca2345ab7f354a27631
sunerok <justinvforvendetta@gmail.com>
2025-07-26 17:43:01 -0400

    remove dbus from dependencies

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -3,7 +3,7 @@ packages:=boost openssl libevent libcap libseccomp zeromq
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_linux_packages:=qt expat dbus libxcb libxcb_util xcb_proto libXau xorgproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_linux_packages:=qt expat libxcb libxcb_util xcb_proto libXau xorgproto freetype fontconfig libX11 xextproto libXext xtrans
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
