c62f7302364bd3424e94ffaf5eb7b769235e8d94
sunerok <justinvforvendetta@gmail.com>
2025-08-18 22:03:12 -0400

    update boost loc

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,16 +1,8 @@
 package=boost
 $(package)_version=1_82_0
-$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source
+$(package)_download_path=https://archives.boost.io/release/1.82.0/source
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
 $(package)_sha256_hash=a6e1ab9b0860e6a2881dd7b21fe9f737a095e5f33a3a874afc6a345228597ee6
-$(package)_patches=
-
-$(package)_compiler=
-ifeq ($(CLANG_ARG),true)
-$(package)_compiler=clang
-else
-$(package)_compiler=gcc
-endif
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
