6e65368f477af28d2f2d81026aad17340cd5da01
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2025-10-30 16:43:42 -0400

    Update native_libtapi.mk to latest commit

--- depends/packages/native_libtapi.mk
+++ depends/packages/native_libtapi.mk
@@ -1,8 +1,8 @@
 package=native_libtapi
-$(package)_version=54c9044082ba35bdb2b0edf282ba1a340096154c
+$(package)_version=aed9334283e3e290bba622ee980bde2322e4d516
 $(package)_download_path=https://github.com/tpoechtrager/apple-libtapi/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=d4d46c64622f13d6938cecf989046d9561011bb59e8ee835f8f39825d67f578f
+$(package)_sha256_hash=F244F7B3D8DE3277204DECE255B531F976BEE80DAD3247E9F36B30052B91C959
 $(package)_patches=disable_zlib.patch no_embed_git_rev.patch
 
 ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
