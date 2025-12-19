9d5d55290e0362feb20f60da339051990458ea7c
sunerok <justinvforvendetta@gmail.com>
2025-12-19 16:55:35 -0500

    new UD api n key

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -54,8 +54,8 @@ static const int MAX_URI_LENGTH = 255;
 #define QAPP_APP_NAME_DEFAULT "verge-qt"
 #define QAPP_APP_NAME_TESTNET "verge-qt-testnet"
 
-#define UNS_API_KEY "Bearer e2bec832-7a87-41cc-bc67-1aac72e2e033"
-#define UNS_API "https://resolve.unstoppabledomains.com/domains/"
+#define UNS_API_KEY "Bearer ri7h_ailrqgaxtfqaqsenkknxnv8zjwmokc6majxsdsxc4eg"
+#define UNS_API "https://api.unstoppabledomains.com/resolve/domains/verge"
 #define UNS_XVG_RECORD "crypto.XVG.address"
 
 #endif // VERGE_QT_GUICONSTANTS_H
