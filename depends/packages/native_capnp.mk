b65e921de381f8d4ccf5a3712ef9698bbde6abb1
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2026-01-04 14:11:20 -0500

    Update native_capnp.mk

--- depends/packages/native_capnp.mk
+++ depends/packages/native_capnp.mk
@@ -1,9 +1,9 @@
 package=native_capnp
-$(package)_version=1.2.0
+$(package)_version=1.3.0
 $(package)_download_path=https://capnproto.org/
 $(package)_download_file=capnproto-$($(package)_version).tar.gz
 $(package)_file_name=capnproto-cxx-$($(package)_version).tar.gz
-$(package)_sha256_hash=ED00E44ECBBDA5186BC78A41BA64A8DC4A861B5F8D4E822959B0144AE6FD42EF
+$(package)_sha256_hash=01AB2BA7F52FCC3C51A10E22935AAE56F3BC5E99B726B7E507FE6700CB12147D
 
 define $(package)_set_vars
   $(package)_config_opts := -DBUILD_TESTING=OFF
