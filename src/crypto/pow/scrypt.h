72e850020067766eaeba0171cbd1625ba844324e
sunerok <justinvforvendetta@gmail.com>
2026-02-16 18:16:44 -0500

    wrap for mac arm

--- src/crypto/pow/scrypt.h
+++ src/crypto/pow/scrypt.h
@@ -38,20 +38,25 @@ PBKDF2_SHA256(const uint8_t *passwd, size_t passwdlen, const uint8_t *salt,
 
 #ifndef __FreeBSD__
 
+#ifndef le32dec
 static inline uint32_t le32dec(const void *pp)
 {
-        const uint8_t *p = (uint8_t const *)pp;
-        return ((uint32_t)(p[0]) + ((uint32_t)(p[1]) << 8) +
-            ((uint32_t)(p[2]) << 16) + ((uint32_t)(p[3]) << 24));
+    const uint8_t *p = (const uint8_t *)pp;
+    return ((uint32_t)(p[0]) +
+           ((uint32_t)(p[1]) << 8) +
+           ((uint32_t)(p[2]) << 16) +
+           ((uint32_t)(p[3]) << 24));
 }
+#endif
 
+#ifndef le32enc
 static inline void le32enc(void *pp, uint32_t x)
 {
-        uint8_t *p = (uint8_t *)pp;
-        p[0] = x & 0xff;
-        p[1] = (x >> 8) & 0xff;
-        p[2] = (x >> 16) & 0xff;
-        p[3] = (x >> 24) & 0xff;
+    uint8_t *p = (uint8_t *)pp;
+    p[0] = x & 0xff;
+    p[1] = (x >> 8) & 0xff;
+    p[2] = (x >> 16) & 0xff;
+    p[3] = (x >> 24) & 0xff;
 }
 #endif
 #endif // VERGE_CRYPTO_POW_SCRYPT_H
