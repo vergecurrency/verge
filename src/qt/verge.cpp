ce81a029f1742bbd75e50aedfed84078691fc950
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2026-02-18 12:58:06 -0500

    fix qt warning on startup
    fixes warning: Attribute Qt::AA_EnableHighDpiScaling must be set before QCoreApplication is created

--- src/qt/verge.cpp
+++ src/qt/verge.cpp
@@ -583,17 +583,18 @@ int main(int argc, char *argv[])
     Q_INIT_RESOURCE(verge);
     Q_INIT_RESOURCE(verge_locale);
 
-    VERGEApplication app(*node, argc, argv);
 #if QT_VERSION > 0x050100
     // Generate high-dpi pixmaps
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #endif
 #if QT_VERSION >= 0x050600
-    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 #ifdef Q_OS_MAC
     QApplication::setAttribute(Qt::AA_DontShowIconsInMenus);
 #endif
+
+    VERGEApplication app(*node, argc, argv);
 
     // Register meta types used for QMetaObject::invokeMethod
     qRegisterMetaType< bool* >();
