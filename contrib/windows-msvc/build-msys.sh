49aa78ed154f1e50b2359bce299cfee1cb7b0f96
sunerok <justinvforvendetta@gmail.com>
2026-03-20 20:01:56 -0400

    contrib(windows-msvc): match protobuf makefile assertion

--- contrib/windows-msvc/build-msys.sh
+++ contrib/windows-msvc/build-msys.sh
@@ -834,7 +834,7 @@ patch_generated_makefiles_for_windows() {
   perl -0pi -e 's/-lQt6WebEngineWidgets/-lQt6WebEngineWidgets -lQt6WebEngineCore/' "$top_makefile"
 
   grep -n '^LIBSECP256K1 = secp256k1/libsecp256k1.la secp256k1/libsecp256k1_precomputed.la$' "$top_makefile" >/dev/null
-  grep -n '^PROTOBUF_LIBS = .*repeated_ptr_field\.cc\.obj .*alternatename:' "$top_makefile" >/dev/null
+  grep -n '^PROTOBUF_LIBS = .*repeated_ptr_field\.cc\.obj .*--verge-protobuf-memswap16-alias' "$top_makefile" >/dev/null
   grep -n '^qt_verge_qt_LDFLAGS = .* -Wl,/subsystem:windows$' "$top_makefile" >/dev/null
   grep -n 'Qt6WebEngineWidgets -lQt6WebEngineCore' "$top_makefile" >/dev/null
 }
