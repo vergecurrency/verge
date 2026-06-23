f99d755929b65837a248a17bc353ecd504a385d4
sunerok <justinvforvendetta@gmail.com>
2026-06-23 12:48:24 -0400

    windows witness fix

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -60,7 +60,6 @@ static constexpr unsigned int STANDARD_SCRIPT_VERIFY_FLAGS = MANDATORY_SCRIPT_VE
                                                              SCRIPT_VERIFY_MINIMALDATA |
                                                              SCRIPT_VERIFY_NULLDUMMY |
                                                              SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_NOPS |
-                                                             SCRIPT_VERIFY_CLEANSTACK |
                                                              SCRIPT_VERIFY_MINIMALIF |
                                                              SCRIPT_VERIFY_NULLFAIL |
                                                              SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY |
